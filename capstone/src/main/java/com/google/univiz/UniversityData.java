5e82fa5702c2857d733c931010eae9715d9d16e3
Bianca Macias <bmac@biancamacias.com>
2020-07-30 23:14:44 +0000

    built university data class

---
+++ capstone/src/main/java/com/google/univiz/UniversityData.java
@@ -0,0 +1,110 @@
+package com.google.univiz;
+
+public final class UniversityData {
+
+  public final class UniversityBuilder {
+    private String uniName;
+    private String uniCity;
+    private float latitude;
+    private float longitude;
+    private int isMainCampus;
+    private int urbanizationDegree;
+    private float admissionRate;
+    private float avgSAT;
+    private int numOfUndergrads;
+    private int avgCostOfAttendance;
+    private int numOfStudentsInDebt;
+    private int carnegieSizeDegree;
+    private float numOfUndergradMen;
+    private float numOfUndergradWomen;
+
+    public UniversityBuilder setName(String name) {
+      this.uniName = name;
+      return this;
+    }
+
+    public UniversityBuilder setCity(String city) {
+      this.uniCity = city;
+      return this;
+    }
+
+    public UniversityBuilder setLat(float lat) {
+      this.latitude = lat;
+      return this;
+    }
+
+    public UniversityBuilder setLong(float log) {
+      this.longitude = log;
+      return this;
+    }
+
+    public UniversityBuilder setMainCampus(int flag) {
+      this.isMainCampus = flag;
+      return this;
+    }
+
+    public UniversityBuilder setUrbanDegree(int degree) {
+      this.urbanizationDegree = degree;
+      return this;
+    }
+
+    public UniversityBuilder setAdmissionRate(float rate) {
+      this.admissionRate = rate;
+      return this;
+    }
+
+    public UniversityBuilder setAvgSAT(int score) {
+      this.avgSAT = score;
+      return this;
+    }
+
+    public UniversityBuilder setNumberOfUndergrads(int size) {
+      this.numOfUndergrads = size;
+      return this;
+    }
+
+    public UniversityBuilder setAvgCost(int cost) {
+      this.avgCostOfAttendance = cost;
+      return this;
+    }
+
+    public UniversityBuilder setStudentsInDebt(int numberOfStudents) {
+      this.numOfStudentsInDebt = numberOfStudents;
+      return this;
+    }
+
+    public UniversityBuilder setCarnegieDegree(int degree) {
+      this.carnegieSizeDegree = degree;
+      return this;
+    }
+
+    public UniversityBuilder setNumOfMen(float numOfMen) {
+      this.numOfUndergradMen = numOfMen;
+      return this;
+    }
+
+    public UniversityBUilder setNumOfWomen(float numOfWomen) {
+      this.numOfUndergradWomen = numOfWomen;
+      return this;
+    }
+
+    public UniversityData build() {
+      UniversityData uni = new UniversityData();
+      uni.uniName = this.uniName;
+      uni.uniCity = this.uniCity;
+      uni.latitude = this.latitude;
+      uni.longitude = this.longitude;
+      uni.isMainCampus = this.isMainCampus;
+      uni.urbanizationDegree = this.urbanizationDegree;
+      uni.admissionRate = this.admissionRate;
+      uni.avgSAT = this.avgSAT;
+      uni.numOfUndergrads = this.setNumberOfUndergrads;
+      uni.avgCostOfAttendance = this.avgCostOfAttendance;
+      uni.numOfStudentsInDebt = this.numOfStudentsInDebt;
+      uni.carnegieSizeDegree = this.carnegieSizeDegree;
+      uni.numOfUndergradMen = this.numOfUndergradMen;
+      uni.numOfUndergradWomen = this.numOfUndergradWomen;
+      return uni;
+    }
+  }
+}
