84fadb93c35c93c81602b38de100a06f4b7d57fc
mmurray22 <michaelamurray2357@gmail.com>
2020-07-30 23:57:48 +0000

    Updates to interface format

--- capstone/src/main/java/com/google/univiz/CollegeScoreCardData/CollegeScoreCardInterface.java
+++ capstone/src/main/java/com/google/univiz/CollegeScoreCardData/CollegeScoreCardInterface.java
@@ -3,6 +3,7 @@ import java.util.List;
 interface CollegeScoreCardInterface {
   /*General Use Functions*/
   public boolean validateCollegeName(String collegeName);
+
   public List<UniversityData> getCollegesData(List<String> collegeNames);
 
   /*Search Feature Functions*/
@@ -10,7 +11,7 @@ interface CollegeScoreCardInterface {
 
   /*Maps Feature Functions*/
   public List<UniversityData> updateMapData(List<UniversityData> collegeDatum);
-  
+
   /*DataVis Feature Functions*/
   public List<UniversityData> updateDataVisData(List<UniversityData> collegeDatum);
 }
