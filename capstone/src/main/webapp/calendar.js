19e8b750cb5545b9162f9dc1c49c11ca653aabc5
Ihsan Olawale <ihsanolawale@outlook.com>
2020-07-31 13:14:46 -0700

    Set up fetching calendar data from servlet

--- capstone/src/main/webapp/calendar.js
+++ capstone/src/main/webapp/calendar.js
@@ -2,24 +2,30 @@ google.charts.load('current', {'packages':['calendar', 'timeline']});
 google.charts.setOnLoadCallback(drawCharts);
 
 function drawCharts() {
-  drawFrequencyChart();
-  drawTimelineChart();
+  const freqData = new google.visualization.DataTable();
+  const timelineData = new google.visualization.DataTable();
+  fetch('/suggested-cal').
+    then((response) => response.json()).
+    then((myObject) => {
+      // update freqData datatable
+      // update timelineData datatable
+    });
+  drawFrequencyChart(freqData);
+  drawTimelineChart(timelineData);
 }
 
-function drawFrequencyChart() {
-  const data = new google.visualization.DataTable();
+function drawFrequencyChart(freqData) {
   const options = {
     'title': 'Expected Application Heat Map'
   }
   const chart = new google.visualization.Calendar(document.getElementById('calendar-freq-map'));
-  chart.draw(data, options);
+  chart.draw(freqData, options);
 }
 
-function drawTimelineChart() {
-  const data = new google.visualization.DataTable();
+function drawTimelineChart(timelineData) {
   const options = {
     'title': 'Suggested Application Timeline'
   }
   const chart = new google.visualization.Timeline(document.getElementById('timeline-chart'));
-  chart.draw(data, options);
+  chart.draw(timelineData, options);
 }
