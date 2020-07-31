6d7594fffa7c4701b26ca1d80e9e6664a8fd61b3
mmurray22 <michaelamurray2357@gmail.com>
2020-07-31 17:03:33 +0000

    Removes interface word from interface

---
+++ capstone/src/main/java/com/google/univiz/collegescorecarddata/CollegeScoreCardInterface.java
@@ -0,0 +1,19 @@
+package com.google.univiz.collegescorecarddata;
+
+import java.util.List;
+
+interface CollegeScoreCard {
+  /*General Use Functions*/
+  boolean validateCollegeName(String collegeName);
+
+  List<UniversityData> getCollegesData(List<String> collegeNames);
+
+  /*Search Feature Functions*/
+  List<UniversityData> getCollegeSuggestions(String currName);
+
+  /*Maps Feature Functions*/
+  List<UniversityData> getMapData(List<UniversityData> collegeDatum);
+
+  /*DataVis Feature Functions*/
+  List<UniversityData> getDataVisData(List<UniversityData> collegeDatum);
+}
