70780b47bb6869beb877aec3358a5dcec79cf07e
Ihsan Olawale <ihsanolawale@outlook.com>
2020-07-31 15:02:15 -0700

    Add JSDoc for parameters and indent properly

--- capstone/src/main/webapp/calendar.js
+++ capstone/src/main/webapp/calendar.js
@@ -19,24 +19,28 @@ function drawCharts() {
 
 /**
  * Generate frequency calendar chart of deadlines.
+ * @param freqData the DataTable corresponding to
+ * the frequency of deadlines
  */
 function drawFrequencyChart(freqData) {
   const options = {
     'title': 'Expected Application Heat Map',
   };
   const chart = new google.visualization.Calendar(
-    document.getElementById('calendar-freq-map'));
+      document.getElementById('calendar-freq-map'));
   chart.draw(freqData, options);
 }
 
 /**
  * Generate suggested timeline of actions related to deadlines.
+ * @param timelineData the DataTable corresponding to the
+ * suggested actions to take before the deadline
  */
 function drawTimelineChart(timelineData) {
   const options = {
     'title': 'Suggested Application Timeline',
   };
   const chart = new google.visualization.Timeline(
-    document.getElementById('timeline-chart'));
+      document.getElementById('timeline-chart'));
   chart.draw(timelineData, options);
 }
