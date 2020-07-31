ff7444e62dd0edcabeb93531727226f709c589b5
Bianca Macias <bmac@biancamacias.com>
2020-07-31 23:05:24 +0000

    added javadoc for variable names

--- capstone/src/main/java/com/google/univiz/UniversityData.java
+++ capstone/src/main/java/com/google/univiz/UniversityData.java
@@ -2,9 +2,33 @@ package com.google.univiz;
 
 import com.google.auto.value.AutoValue;
 
+/** UniversityData is a class representing a single university. */
+
 @AutoValue
 public abstract class UniversityData {
 
+  /**
+   * Creates a new UniversityData
+   *
+   * @param name The human-readable name for the school/college/university.
+   * @param city The human-readable name for the city the university is located in.
+   * @param latitude A float representing the school's location measured in latitude.
+   * @param longitude A float representing the school's location measured in longitude.
+   * @param flagMainCampus An integer that flags whether the university is the main campus or not, 1
+   *     or 0.
+   * @param urbanizationDegree An integer from range(1-8) that tells the degree of urbanization for
+   *     that campus, from large city to rural.
+   * @param admissionRate The overall rate of admission for the university.
+   * @param avgSat The average overall SAT score for students accepted to the university.
+   * @param numOfUndergrads The number of undergraduate students that attend the university.
+   * @param avgCost The average cost of attendance for students at the university.
+   * @param studentsInDebt The number of students in cumulative loan debt after graduation.
+   * @param carnegieSizeDegree An integer from range(-2-18) that tells degree of Carnegie size, from
+   *     not applicable to exclusively graduate/professional.
+   * @param numOfMen A float that represents the number of men enrolled at the undergraduate level.
+   * @param numOfWomen A float that represents the number of women enrolled at the undergradute
+   *     level.
+   */
   public static UniversityData create(
       String name,
       String city,
@@ -13,7 +37,7 @@ public abstract class UniversityData {
       int flagMainCampus,
       int urbanizationDegree,
       float admissionRate,
-      float avgSAT,
+      float avgSat,
       int numOfUndergrads,
       int avgCost,
       int studentsInDebt,
@@ -28,7 +52,7 @@ public abstract class UniversityData {
         flagMainCampus,
         urbanizationDegree,
         admissionRate,
-        avgSAT,
+        avgSat,
         numOfUndergrads,
         avgCost,
         studentsInDebt,
@@ -51,7 +75,7 @@ public abstract class UniversityData {
 
   public abstract float admissionRate();
 
-  public abstract float avgSAT();
+  public abstract float avgSat();
 
   public abstract int numOfUndergrads();
 
