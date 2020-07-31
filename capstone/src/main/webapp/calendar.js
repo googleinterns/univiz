e75016ccd97e40334ab7822a49f6af4fb93e30c0
Ihsan Olawale <ihsanolawale@outlook.com>
2020-07-31 14:53:44 -0700

    Address linter errors and add JSDocs

--- capstone/src/main/webapp/calendar.js
+++ capstone/src/main/webapp/calendar.js
@@ -1,31 +1,42 @@
-google.charts.load('current', {'packages':['calendar', 'timeline']});
+google.charts.load('current', {'packages': ['calendar', 'timeline']});
 google.charts.setOnLoadCallback(drawCharts);
 
+/**
+ * Callback function to generate charts.
+ */
 function drawCharts() {
   const freqData = new google.visualization.DataTable();
   const timelineData = new google.visualization.DataTable();
   fetch('/suggested-cal').
-    then((response) => response.json()).
-    then((myObject) => {
-      // update freqData datatable
-      // update timelineData datatable
-    });
+      then((response) => response.json()).
+      then((myObject) => {
+        // update freqData datatable
+        // update timelineData datatable
+      });
   drawFrequencyChart(freqData);
   drawTimelineChart(timelineData);
 }
 
+/**
+ * Generate frequency calendar chart of deadlines.
+ */
 function drawFrequencyChart(freqData) {
   const options = {
-    'title': 'Expected Application Heat Map'
-  }
-  const chart = new google.visualization.Calendar(document.getElementById('calendar-freq-map'));
+    'title': 'Expected Application Heat Map',
+  };
+  const chart = new google.visualization.Calendar(
+    document.getElementById('calendar-freq-map'));
   chart.draw(freqData, options);
 }
 
+/**
+ * Generate suggested timeline of actions related to deadlines.
+ */
 function drawTimelineChart(timelineData) {
   const options = {
-    'title': 'Suggested Application Timeline'
-  }
-  const chart = new google.visualization.Timeline(document.getElementById('timeline-chart'));
+    'title': 'Suggested Application Timeline',
+  };
+  const chart = new google.visualization.Timeline(
+    document.getElementById('timeline-chart'));
   chart.draw(timelineData, options);
 }
