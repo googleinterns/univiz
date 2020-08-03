bb7110973ff090a089463879520e07b8ae498043
Bianca Macias <bmac@biancamacias.com>
2020-08-03 21:58:58 +0000

    added SerializedName annotation to class imports

--- capstone/src/main/java/com/google/univiz/scorecard/ScorecardData.java
+++ capstone/src/main/java/com/google/univiz/scorecard/ScorecardData.java
@@ -1,6 +1,7 @@
 package com.google.univiz.scorecard;
 
 import com.google.auto.value.AutoValue;
+import com.google.gson.annotations.SerializedName;
 
 /**
  * ScorecardData is a class representing a single university/college/institution from the parsed
