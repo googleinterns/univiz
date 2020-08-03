616cf0194e5a885bbd6b81bf01a74cd6ad96768e
mmurray22 <michaelamurray2357@gmail.com>
2020-08-03 17:33:39 +0000

    Formats the java

--- capstone/src/main/java/com/google/univiz/api/MapData.java
+++ capstone/src/main/java/com/google/univiz/api/MapData.java
@@ -1,12 +1,12 @@
 package com.google.univiz.api;
 
 /*
- * The MapData interface is responsible for all data management/processing specific for the maps 
+ * The MapData interface is responsible for all data management/processing specific for the maps
  * feature of the UniViz webapp.
  */
 public interface MapData {
   /*
-   * Takes a list of college names and returns a CollegeMapsData object to the caller. This object 
+   * Takes a list of college names and returns a CollegeMapsData object to the caller. This object
    * contains all data relevant to the Maps feature.
    */
   CollegeMapsData getMapData(List<String> collegeNames);
