b7c414f975b88ff696d1103c595c74122dc70292
mmurray22 <michaelamurray2357@gmail.com>
2020-08-03 21:05:49 +0000

    Adds JavaDoc commenting on Maps interface

--- capstone/src/main/java/com/google/univiz/api/MapData.java
+++ capstone/src/main/java/com/google/univiz/api/MapData.java
@@ -8,6 +8,12 @@ public interface MapData {
   /*
    * Takes a list of college names and returns a CollegeMapsData object to the caller. This object
    * contains all data relevant to the Maps feature.
+   *
+   * @args Takes a list of CollegeIDs, which is a class with two components: a college name and a 
+   * unique college id
+   *
+   * @return Returns a list of CollegeMapsData objects which contain all information necessary for
+   * a geographic visualization 
    */
-  List<CollegeMapsData> getMapData(List<String> collegeNames);
+  List<CollegeMapsData> getMapData(List<CollegeID> colleges);
 }
