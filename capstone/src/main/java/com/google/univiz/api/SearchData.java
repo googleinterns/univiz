7aaa8d3e52a048e02c7ac829a9fab4efe1ff26f6
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-08-03 11:21:15 -0700

    Update capstone/src/main/java/com/google/univiz/api/SearchData.java
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>

--- capstone/src/main/java/com/google/univiz/api/SearchData.java
+++ capstone/src/main/java/com/google/univiz/api/SearchData.java
@@ -6,7 +6,7 @@ public interface SearchData {
   /**
    * This function take a potential college name fragment (provided by the user) and uses the API in
    * use to determine whether this string fragment is present in any college names within the API
-   * database; If so, then a list of college name containing the provided string fragment
+   * database; If so, then returns a list of college name containing the provided string fragment.
    */
   List<String> getCollegeSuggestions(String currentCollegeNameFragment);
 }
