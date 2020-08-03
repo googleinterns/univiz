6a7276120d5077204a96236caf21cdbcab947e9f
Bianca Macias <bmac@biancamacias.com>
2020-08-03 22:35:07 +0000

    added line for sample json

--- capstone/src/test/resources/com/google/univiz/scorecard/ScorecardTest.java
+++ capstone/src/test/resources/com/google/univiz/scorecard/ScorecardTest.java
@@ -10,6 +10,7 @@ public final class ScorecardTest {
   @Test
   public void parseJsonTest() {
     // TODO(biancamacias): insert sample JSON
+    String json = "{}";
     ScorecardData college = new Gson().fromJson(json, ScorecardData.class);
 
     asserThat(college.name().isEqualTo("New York University"));
