e3c70b4c792041bc5e2ffb9c36cb6dee6bf333cd
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-04 16:43:54 -0700

    Address formatting issue

--- capstone/src/test/javascript/graphsTest.js
+++ capstone/src/test/javascript/graphsTest.js
@@ -1,8 +1,8 @@
 describe('Data Table Construction', function() {
   it('Will add correct columns/rows to DataTable from JSON', function() {
-
-    window.initMap = function() {
-      // TODO(ihsan314): Initialize this when format of JSON object is well-defined
+    window.initTuitionData = function() {
+      // TODO(ihsan314): Initialize this when format of
+      // JSON object is well-defined
       const mockTuitionInfo = {};
       const expectedData = new google.visualization.DataTable();
       const actualData = new google.visualization.DataTable();
