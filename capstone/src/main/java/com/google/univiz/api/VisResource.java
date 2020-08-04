ae4cf541cfdb7575d3e0a3fa89bc2154fcd5cbb0
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-08-04 14:14:42 -0700

    Adds javadoc to VisResource

--- capstone/src/main/java/com/google/univiz/api/VisResource.java
+++ capstone/src/main/java/com/google/univiz/api/VisResource.java
@@ -1,4 +1,7 @@
 package com.google.univiz.api;
 
-/** */
+/**
+ * The VisResource interface is responsible for all data management/processing specific for the vis
+ * feature of the UniViz webapp.
+ */
 public interface VisResource {}
