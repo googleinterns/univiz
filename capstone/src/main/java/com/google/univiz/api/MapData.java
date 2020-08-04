5f1fbb783c66c3017695ac3701a4beec13a01b12
mmurray22 <michaelamurray2357@gmail.com>
2020-08-04 21:06:17 +0000

    Adds some more JavaDoc to the comments

--- capstone/src/main/java/com/google/univiz/api/MapData.java
+++ capstone/src/main/java/com/google/univiz/api/MapData.java
@@ -9,7 +9,7 @@ public interface MapData {
    * Takes a list of college names and returns a CollegeMapsData object to the caller. This object
    * contains all data relevant to the Maps feature.
    *
-   * @args Takes a list of CollegeIDs, which is a class with two components: a college name and a 
+   * @param Takes a list of CollegeIDs, which is a class with two components: a college name and a 
    * unique college id
    *
    * @return Returns a list of CollegeMapsData objects which contain all information necessary for
