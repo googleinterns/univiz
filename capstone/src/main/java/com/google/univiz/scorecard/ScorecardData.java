dd8da451c76ac7e19998fa1ea9abbd330326a8c9
Bianca Macias <bmac@biancamacias.com>
2020-08-04 20:32:43 +0000

    formatting nits

--- capstone/src/main/java/com/google/univiz/scorecard/ScorecardData.java
+++ capstone/src/main/java/com/google/univiz/scorecard/ScorecardData.java
@@ -3,7 +3,7 @@ package com.google.univiz.scorecard;
 import com.google.auto.value.AutoValue;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.TypeAdapter;
-import com.ryanharter.auto.value.gson.GenerateTypeAdapter
+import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
 
 /**
  * ScorecardData is a class representing a single university/college/institution from the parsed
