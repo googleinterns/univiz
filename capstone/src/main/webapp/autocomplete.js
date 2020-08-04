109b9499c159960db94176e7692d6f195f48364b
mmurray22 <michaelamurray2357@gmail.com>
2020-08-04 18:50:51 +0000

    Fixes more JS Linter problems

--- capstone/src/main/webapp/autocomplete.js
+++ capstone/src/main/webapp/autocomplete.js
@@ -8,26 +8,26 @@
  * Link: https://www.w3schools.com/howto/howto_js_autocomplete.asp
 */
 function autocomplete(inp, arr) {
-  var currentFocus;
-  inp.addEventListener("input", function(e) {
+  let currentFocus;
+  inp.addEventListener('input', function(e) {
     var a, b, i, val = this.value;
     closeAllLists();
     if (!val) { 
         return false;
     }
     currentFocus = -1;
-    a = document.createElement("DIV");
-    a.setAttribute("id", this.id + "autocomplete-list");
-    a.setAttribute("class", "autocomplete-items");
+    a = document.createElement('DIV');
+    a.setAttribute('id', this.id + 'autocomplete-list');
+    a.setAttribute('class', 'autocomplete-items');
     this.parentNode.appendChild(a);
     for (i = 0; i < arr.length; i++) {
       if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-        b = document.createElement("DIV");
-        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b = document.createElement('DIV');
+        b.innerHTML = '<strong>' + arr[i].substr(0, val.length) + '</strong>';
         b.innerHTML += arr[i].substr(val.length);
-        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-        b.addEventListener("click", function(e) {
-          inp.value = this.getElementsByTagName("input")[0].value;
+        b.innerHTML += '<input type='+'hidden'+'value='+arr[i]+'>';
+        b.addEventListener('click', function(e) {
+          inp.value = this.getElementsByTagName('input')[0].value;
           closeAllLists();
         });
         a.appendChild(b);
@@ -35,10 +35,10 @@ function autocomplete(inp, arr) {
     }
   });
 
-  inp.addEventListener("keydown", function(e) {
-    var x = document.getElementById(this.id + "autocomplete-list");
+  inp.addEventListener('keydown', function(e) {
+    let x = document.getElementById(this.id + 'autocomplete-list');
     if (x) {
-      x = x.getElementsByTagName("div");
+      x = x.getElementsByTagName('div');
     }
     if (e.keyCode == 40) {
       currentFocus++;
@@ -66,22 +66,22 @@ function autocomplete(inp, arr) {
     if (currentFocus < 0) {
       currentFocus = (x.length - 1);
     }
-    x[currentFocus].classList.add("autocomplete-active");
+    x[currentFocus].classList.add('autocomplete-active');
   }
   function removeActive(x) {
     for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
+      x[i].classList.remove('autocomplete-active');
     }
   }
   function closeAllLists(elmnt) {
-    var x = document.getElementsByClassName("autocomplete-items");
+    var x = document.getElementsByClassName('autocomplete-items');
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
         x[i].parentNode.removeChild(x[i]);
       }
     }
   }
-  document.addEventListener("click", function (e) {
+  document.addEventListener('click', function (e) {
     closeAllLists(e.target);
   });
 }
