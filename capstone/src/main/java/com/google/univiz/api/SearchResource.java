e0ea49b67c9a1dc8c9c690eea3b39bfb52dd66ce
mmurray22 <michaelamurray2357@gmail.com>
2020-08-04 21:20:05 +0000

    Fixes formatting of interfaces

--- capstone/src/main/java/com/google/univiz/api/SearchResource.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResource.java
@@ -1,7 +1,7 @@
 package com.google.univiz.api;
 
 /**
- * The SearchResoure interface is responsible for all data management/processing specific for the 
+ * The SearchResoure interface is responsible for all data management/processing specific for the
  * search feature of the UniViz webapp.
  */
 public interface SearchResource {}
---
+++ capstone/src/main/java/com/google/univiz/api/fix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Formats a Java source file using https://github.com/google/google-java-format
+#
+# COMMIT OFTEN AND USE AT YOUR OWN RISK.
+
+JAVA_FORMATTER_JAR=https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar
+LOCAL_JAVA_FORMATTER_PATH=/tmp/google-java-format-1.7-all-deps.jar
+
+wget -q -nc -O $LOCAL_JAVA_FORMATTER_PATH $JAVA_FORMATTER_JAR
+
+java -jar $LOCAL_JAVA_FORMATTER_PATH --replace $1 
