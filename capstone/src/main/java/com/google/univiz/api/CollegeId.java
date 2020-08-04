079a397e93e1e23c433c15b4c956a693bc440c83
mmurray22 <michaelamurray2357@gmail.com>
2020-08-04 21:10:38 +0000

    Adds JavaDoc to new objects

--- capstone/src/main/java/com/google/univiz/api/CollegeId.java
+++ capstone/src/main/java/com/google/univiz/api/CollegeId.java
@@ -1,4 +1,7 @@
 package com.google.univiz.api;
 
-/** */
+/**
+ * The CollegeId class will contain a college name and a unique college ID that can be used to 
+ * identify the college
+ */
 public class CollegeId {}
--- capstone/src/main/java/com/google/univiz/api/CollegeMapsData.java
+++ capstone/src/main/java/com/google/univiz/api/CollegeMapsData.java
@@ -1,4 +1,6 @@
 package com.google.univiz.api;
 
-/** */
+/** 
+ * The CollegeMapsData class will contain all college data that is specific to the Maps feature 
+ */
 public class CollegeMapsData {}
