20ff76d7a66f342d86130f2a642a978ef63eac41
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-04 17:57:41 -0700

    Move initialization out of callback, remove extraneous code

--- capstone/src/test/javascript/calendarTest.js
+++ capstone/src/test/javascript/calendarTest.js
@@ -1,21 +1,20 @@
 describe('', function() {
   it('', function() {
-    const script = document.createElement('script');
-    script.src = 'https://www.gstatic.com/charts/loader.js';
-    script.defer = true;
-
+    const mockCalenderInfo = {};
+    const expectedFreqData;
+    const expectedTimelineData;
+    const actualFreqData;
+    const actualTimelineData;
     window.initMap = function() {
       // TODO(ihsan314): adjust table and mock calendar info
       // when format of JSON is confirmed
-      const mockCalenderInfo = {};
-      const expectedFreqData = new google.visualization.DataTable();
-      const expectedTimelineData = new google.visualization.DataTable();
-      const actualFreqData = new google.visualization.DataTable();
-      const actualTimelineData = new google.visualization.DataTable();
+      expectedFreqData = new google.visualization.DataTable();
+      expectedTimelineData = new google.visualization.DataTable();
+      actualFreqData = new google.visualization.DataTable();
+      actualTimelineData = new google.visualization.DataTable();
       populateDataTables(actualFreqData, actualTimelineData, mockCalenderInfo);
-      expect(actualFreqData).toBe(expectedFreqData);
-      expect(actualTimelineData).toBe(expectedTimelineData);
     };
-    document.head.appendChild(script);
+    expect(actualFreqData).toBe(expectedFreqData);
+    expect(actualTimelineData).toBe(expectedTimelineData);
   });
 });
