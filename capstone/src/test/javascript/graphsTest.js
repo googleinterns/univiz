455973602db71ed1c8bd09ec1fadc7223d00fdf0
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-04 17:45:27 -0700

    Moves declaration of objects out of callback

--- capstone/src/test/javascript/graphsTest.js
+++ capstone/src/test/javascript/graphsTest.js
@@ -1,13 +1,15 @@
 describe('Data Table Construction', function() {
   it('Will add correct columns/rows to DataTable from JSON', function() {
+    const expectedData;
+    const actualData;
+    const mockTuitionInfo = {};
     window.initTuitionData = function() {
       // TODO(ihsan314): Initialize this when format of
       // JSON object is well-defined
-      const mockTuitionInfo = {};
-      const expectedData = new google.visualization.DataTable();
-      const actualData = new google.visualization.DataTable();
+      expectedData = new google.visualization.DataTable();
+      actualData = new google.visualization.DataTable();
       populateDataTable(actualData, mockTuitionInfo);
-      expect(actualData).toBe(expectedData);
     };
+    expect(actualData).toBe(expectedData);
   });
 });
