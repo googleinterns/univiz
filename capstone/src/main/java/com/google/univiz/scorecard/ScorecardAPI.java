d15568a6c5adeb22ac2412ebbcbb7547c50b44a7
Bianca Macias <bmac@biancamacias.com>
2020-08-05 22:30:37 +0000

    removed incorrect javadoc

--- capstone/src/main/java/com/google/univiz/scorecard/ScorecardAPI.java
+++ capstone/src/main/java/com/google/univiz/scorecard/ScorecardAPI.java
@@ -5,24 +5,7 @@ import java.util.Set;
 
 public interface CollegeData {
 
-  /** TODO(biancamacias): decide what getSuggestions does */
   List<ScorecardData> getSuggestions(String query);
 
-  /**
-   * Returns a list of ScorecardData to be used by the servlets.
-   *
-   * @param ids A list of college IDs that were selected by the user.
-   * @param fieldNames A list of fieldNames of data requested that will be used to call the
-   *     CollegeScorecard API?
-   * @return A list of ScorecardData types to be used in the back-end.
-   */
   List<ScorecardData> getCollegesById(Set<String> ids, Set<String> fieldNames);
-
-  /**
-   * Returns CollegeScorecard API call as JSON.
-   *
-   * @param ids A list of college IDs that were selected by the user.
-   * @param fieldNames A list of fieldNames of data that will be used in the URL that calls the API.
-   * @return A string that holds the JSON response.
-   */
 }
