4ca4256175408e0dd7d7b21ac75c4cae3b8c7a37
mmurray22 <michaelamurray2357@gmail.com>
2020-08-05 23:15:22 +0000

    Changes variable names to make them more concise

--- capstone/src/main/java/com/google/univiz/api/CollegeMapsData.java
+++
@@ -1,6 +0,0 @@
-package com.google.univiz.api;
-
-/** 
- * The CollegeMapsData class will contain all college data that is specific to the Maps feature 
- */
-public class CollegeMapsData {}
--- capstone/src/main/java/com/google/univiz/api/MapResource.java
+++ capstone/src/main/java/com/google/univiz/api/MapResource.java
@@ -1,13 +1,13 @@
 package com.google.univiz.api;
 
 /**
- * The MapData interface is responsible for all data management/processing specific for the maps
+ * The MapResource interface is responsible for all data management/processing specific for the maps
  * feature of the UniViz webapp.
  */
 public interface MapResource {
   /**
-   * Takes a list of college names and returns a list of MapsData objects to the caller. This 
-   * object contains all data relevant to the Maps feature.
+   * Takes a list of college names and returns a list of MapsData objects to the caller. This object 
+   * contains all data relevant to the Maps feature.
    */
   List<MapsData> getMapData(List<CollegeId> colleges);
 }
