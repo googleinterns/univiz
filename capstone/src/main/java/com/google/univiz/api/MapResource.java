1f7e25891d28e597419c56de4bde7117477ab1b8
mmurray22 <michaelamurray2357@gmail.com>
2020-08-05 22:55:36 +0000

    Makes MapResource the main map interface

--- capstone/src/main/java/com/google/univiz/api/MapResource.java
+++ capstone/src/main/java/com/google/univiz/api/MapResource.java
@@ -1,7 +1,13 @@
 package com.google.univiz.api;
 
 /**
- * The MapResource interface is responsible for all data management/processing specific for the maps
+ * The MapData interface is responsible for all data management/processing specific for the maps
  * feature of the UniViz webapp.
  */
-public interface MapResource {}
+public interface MapResource {
+  /**
+   * Takes a list of college names and returns a list of MapsData objects to the caller. This 
+   * object contains all data relevant to the Maps feature.
+   */
+  List<MapsData> getMapData(List<CollegeId> colleges);
+}
