7a85323571f908abac54dae74cb1de059bea4b00
mmurray22 <michaelamurray2357@gmail.com>
2020-08-05 20:38:42 +0000

    Fixes to MapData

--- capstone/src/main/java/com/google/univiz/api/MapData.java
+++ capstone/src/main/java/com/google/univiz/api/MapData.java
@@ -8,11 +8,6 @@ public interface MapData {
   /**
    * Takes a list of college names and returns a list of CollegeMapsData objects to the caller. This 
    * object contains all data relevant to the Maps feature.
-   *
-   * @param Takes a list of CollegeIDs which represents a list of unique colleges
-   *
-   * @return Returns a list of CollegeMapsData objects which contain all information necessary for
-   * a geographic visualization 
    */
   List<CollegeMapsData> getMapData(List<CollegeId> colleges);
 }
