f63c29d3902b35ffa266f680fd5a9bffcb2d891c
Bianca Macias <bmac@biancamacias.com>
2020-08-06 18:52:36 +0000

    added ScorecardConverter class

---
+++ capstone/src/main/java/com/google/univiz/scorecard/ScorecardConverter.java
@@ -0,0 +1,16 @@
+package com.google.univiz.scorecard;
+
+import com.google.common.base.Converter;
+import java.util.List;
+
+public class ScorecardConverter {
+
+    public CollegeData convert() {
+        CollegeData college = new CollegeData();
+        Converter<ScorecardData, CollegeData> converter = new Converter();
+
+    }
+
+    
+}
+
