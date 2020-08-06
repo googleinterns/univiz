1d1061804b2bc9003da2aae7e9b850c6fe3c3823
mmurray22 <michaelamurray2357@gmail.com>
2020-08-06 23:29:34 +0000

    Formats CollegeId

--- capstone/src/main/java/com/google/univiz/api/CollegeId.java
+++ capstone/src/main/java/com/google/univiz/api/CollegeId.java
@@ -1,8 +1,8 @@
 package com.google.univiz.api;
 
 /**
- * This data structure contains all the college fields that are needed to uniquely identify a college. If two
- *  colleges have the same id, they they are the same college. Furthermore this structure may contain
- *  other alternative fields that could be used to uniquely identify a college.
+ * This data structure contains all the college fields that are needed to uniquely identify a
+ * college. If two colleges have the same id, they they are the same college. Furthermore this
+ * structure may contain other alternative fields that could be used to uniquely identify a college.
  */
 public class CollegeId {}
