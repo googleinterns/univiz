fd8fae4363b80b2ae778747f850212b5de4636c6
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-06 14:53:25 -0700

    Set up visualization interface and datatypes

--- capstone/src/main/java/com/google/univiz/api/VisResource.java
+++ capstone/src/main/java/com/google/univiz/api/VisResource.java
@@ -1,7 +1,19 @@
 package com.google.univiz.api;
 
+import com.google.univiz.api.dataviz.Deadline;
+import com.google.univiz.api.dataviz.Timeline;
+import com.google.univiz.api.dataviz.TuitionStats;
+import java.util.List;
+
 /**
  * The VisResource interface is responsible for all data management/processing specific for the vis
  * feature of the UniViz webapp.
  */
-public interface VisResource {}
+public interface VisResource {
+
+  List<TuitionStats> getTuitionStats(List<String> colleges);
+
+  List<Timeline> getRecommendedTimeline(List<String> colleges);
+
+  List<Deadline> getDeadlines(List<String> colleges);
+}
---
+++ capstone/src/main/java/com/google/univiz/api/dataviz/TuitionStats.java
@@ -0,0 +1,4 @@
+package com.google.univiz.api.dataviz;
+
+public class TuitionStats {
+}
