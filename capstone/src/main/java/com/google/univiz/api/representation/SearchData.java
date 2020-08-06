7712cf02f869aa258ae622bfdd0e1c4ba700a202
mmurray22 <michaelamurray2357@gmail.com>
2020-08-06 22:31:41 +0000

    Removes irrelevant variables from SearchData

--- capstone/src/main/java/com/google/univiz/api/representation/SearchData.java
+++ capstone/src/main/java/com/google/univiz/api/representation/SearchData.java
@@ -5,9 +5,6 @@ import com.google.auto.value.AutoValue;
 /** SearchData represents the college data specific to the Search feature */
 @AutoValue
 public abstract class SearchData {
-  private String collegeName;
-  private int collegeId;
-
   public static SearchData create(String collegeName, int collegeId) {
     return new AutoValue_SearchData(collegeName, collegeId);
   }
