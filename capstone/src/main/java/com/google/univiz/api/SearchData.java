30dc29f78c68be4fe0b8e437648150c89190aa65
mmurray22 <michaelamurray2357@gmail.com>
2020-08-06 05:38:59 +0000

    Adds dummy implementation for SuggestionData

--- capstone/src/main/java/com/google/univiz/api/SearchData.java
+++ capstone/src/main/java/com/google/univiz/api/SearchData.java
@@ -1,4 +1,12 @@
 package com.google.univiz.api;
 
 /** SearchData represents the college data specific to the Search feature */
-public class SearchData {}
+public class SearchData {
+  String collegeName;
+  int collegeId;
+
+  public SearchData(String collegeName, int collegeId) {
+    this.collegeName = collegeName;
+    this.collegeId = collegeId;
+  }
+}
--- capstone/src/main/java/com/google/univiz/api/SuggestionData.java
+++ capstone/src/main/java/com/google/univiz/api/SuggestionData.java
@@ -1,4 +1,20 @@
 package com.google.univiz.api;
 
 /** THIS IS A DUMMY CLASS TO ALLOW COMPILATION: REMOVE BEFORE MERGING */
-public class SuggestionData {}
+public class SuggestionData {
+  String collegeName;
+  int collegeId;
+
+  public SuggestionData(String collegeName, int collegeId) {
+    this.collegeName = collegeName;
+    this.collegeId = collegeId;
+  }
+  
+  public String getCollegeName() {
+    return collegeName;
+  }
+  
+  public int getCollegeId() {
+    return collegeId;
+  }
+}
