1834a212b392e1171c72f98800853f615654802e
mmurray22 <michaelamurray2357@gmail.com>
2020-08-06 16:46:57 +0000

    Changes to SearchResourceImpl for compilation

--- capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
@@ -9,7 +9,7 @@ public class SearchResourceImpl extends SearchResource {
   public List<SearchData> getSearchSuggestions(List<SuggestionData> collegeNames) {
     List<SearchData> searchDataList = new ArrayList<SearchData>();
     for (SuggestionData college : collegeNames) {
-      SearchData searchDataObj = new SearchData(college.getName(), college.getCollegeId());
+      SearchData searchDataObj = new SearchData(college.getCollegeName(), college.getCollegeId());
       searchDataList.add(searchDataObj);
     }
     return searchDataList;
