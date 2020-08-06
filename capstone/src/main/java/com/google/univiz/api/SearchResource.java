ef92995211f5b7506b202d4463c62c9f0088f2cd
mmurray22 <michaelamurray2357@gmail.com>
2020-08-06 02:06:32 +0000

    Updates SearchResource java file

--- capstone/src/main/java/com/google/univiz/api/SearchResource.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResource.java
@@ -1,7 +1,15 @@
 package com.google.univiz.api;
 
+import java.util.List;
+
 /**
  * The SearchResoure interface is responsible for all data management/processing specific for the
  * search feature of the UniViz webapp.
  */
-public interface SearchResource {}
+public interface SearchResource {
+  /**
+   * Take a list of suggestion data and returns a list of Suggestions objects to the caller. This 
+   * object contains all data relevant to the Search feature.
+   */
+  List<Suggestion> getSearchSuggestions(List<SuggestionData> collegeNames);
+}
