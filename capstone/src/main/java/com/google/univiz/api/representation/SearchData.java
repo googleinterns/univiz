301669a21e03edd7a0a3ab9cb73344bf144b2d22
mmurray22 <michaelamurray2357@gmail.com>
2020-08-06 20:39:49 +0000

    Fixes autovalue inconsistencies

--- capstone/src/main/java/com/google/univiz/api/representation/SearchData.java
+++ capstone/src/main/java/com/google/univiz/api/representation/SearchData.java
@@ -4,13 +4,17 @@ import com.google.auto.value.AutoValue;
 
 /** SearchData represents the college data specific to the Search feature */
 @AutoValue
-abstract class SearchData {
+public abstract class SearchData {
   private String collegeName;
   private int collegeId;
 
-  static SearchData create(String collegeName, int collegeId) {
+  public static SearchData create(String collegeName, int collegeId) {
     this.collegeName = collegeName;
     this.collegeId = collegeId;
     return AutoValue_SearchData(collegeName, collegeId);
   }
+
+  public abstract String getCollegeName();
+
+  public abstract int getCollegeId();
 }
