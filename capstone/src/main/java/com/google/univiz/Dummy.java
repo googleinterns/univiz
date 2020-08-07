4ab67a15f6295acba2c6e4d0c182b10998471fcb
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-07 16:18:41 -0700

    Test class with terrible formatting

---
+++ capstone/src/main/java/com/google/univiz/Dummy.java
@@ -0,0 +1,13 @@
+package com.google.univiz;
+
+import java.util.List;
+import java.util.ArrayList; // intentionally placed in non-alphabetical order
+
+public class Dummy {
+	// deep indentation of eight spaces instead of the regular two.
+	// At last, today is the day where I intentionally write over eighty-five characters in a line.
+	// I usually avoid doing that, but today it has to be done.
+
+	List<Integer> myList = new ArrayList<>();
+}
+
