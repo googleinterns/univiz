ca05433aff541b70ad84091871efe6e91e2b0358
Chris Condit <ccondit@google.com>
2020-08-07 15:51:05 -0700

    Trues up line lengths to pass lint.

--- univiz.conf.js
+++ univiz.conf.js
@@ -25,8 +25,8 @@ module.exports = function(config) {
     ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    // preprocess matching files before serving them to the browser available 
+    // preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
     },
 
@@ -44,18 +44,19 @@ module.exports = function(config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || 
+    //                  config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
+    // enable / disable watching file and executing tests whenever any file
+    // changes
     autoWatch: true,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    // start these browsers available browser launchers: 
+    // https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
 
