6dc399f40e44757c9038e8a6e1d5f4ebebec328e
mmurray22 <michaelamurray2357@gmail.com>
2020-08-07 17:06:17 +0000

    Removes changes to VisResource/VisData

--- capstone/src/main/java/com/google/univiz/api/VisData.java
+++
@@ -1,4 +0,0 @@
-package com.google.univiz.api;
-
-/** VisData represents the college data specific to the Vis feature */
-public class VisData {}
--- capstone/src/main/java/com/google/univiz/api/VisResource.java
+++ capstone/src/main/java/com/google/univiz/api/VisResource.java
@@ -1,15 +1,8 @@
 package com.google.univiz.api;
 
-import java.util.List;
 
 /**
  * The VisResource interface is responsible for all data management/processing specific for the vis
  * feature of the UniViz webapp.
  */
-public interface VisResource {
-  /**
-   * Take a list of suggestion data and returns a list of VisData object to the caller. This object
-   * contains all data relevant to the Vis feature.
-   */
-  List<VisData> getVisData(List<CollegeData> collegeNames);
-}
+public interface VisResource {}
