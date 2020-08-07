731f1c11cac28aa468765160b7fa3c7ab852531f
Bianca Macias <bmac@biancamacias.com>
2020-08-07 17:42:00 +0000

    updated javadoc for CollegeDataApi interface

--- capstone/src/main/java/com/google/univiz/CollegeDataApi.java
+++ capstone/src/main/java/com/google/univiz/CollegeDataApi.java
@@ -4,7 +4,8 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * The CollegeDataApi
+ * The CollegeDataApi interface is responsible for all REST requests and JSON parsing to the
+ * CollegeData class. CollegeDataApi is the only class that will communicate with the external API.
  */
 public interface CollegeDataApi {
 
