ae343bbcb90fb27e6822fd097045c30a74d5cf9c
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-07 11:09:45 -0700

    Format files in com.google.univiz.api.dataviz

--- capstone/src/main/java/com/google/univiz/api/dataviz/Timeline.java
+++ capstone/src/main/java/com/google/univiz/api/dataviz/Timeline.java
@@ -1,9 +1,8 @@
 package com.google.univiz.api.dataviz;
 
 /**
- * Contains a series of recommended actions and recommended timespans
- * for each action, for a particular college. Actions consist of writing
- * essays for applications, getting essays reviewed, and looking into
- * getting financial aid.
+ * Contains a series of recommended actions and recommended timespans for each action, for a
+ * particular college. Actions consist of writing essays for applications, getting essays reviewed,
+ * and looking into getting financial aid.
  */
 public class Timeline {}
