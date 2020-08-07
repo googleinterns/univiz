2740997bb4c3851d3b537f0efc9f37330f67c0fc
mmurray22 <michaelamurray2357@gmail.com>
2020-08-07 18:24:23 +0000

    Adds Suggestion API functions

---
+++ capstone/src/main/java/com/google/univiz/SuggestionAPIData.java
@@ -0,0 +1,15 @@
+package com.google.univiz;
+
+import java.util.List;
+
+/**
+ * Communicates directly with the REST API to get a list of college suggestions related to a client
+ * query
+ */
+public interface SuggestionDataAPI {
+  /**
+   * Takes a list of college names and returns a list of SuggestionData objects, where each object
+   * is unique to each college name providede in the first list
+   */
+  List<SuggestionsData> getCollegeSuggestions(List<String> collegeNames);
+}
