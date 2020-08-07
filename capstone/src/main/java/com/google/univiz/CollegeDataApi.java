7e7465de7c19e8ae16ea9715fbd3b6b35c76f2a6
Bianca Macias <bmac@biancamacias.com>
2020-08-07 01:14:21 +0000

    removed names from getCollegesById in interface

--- capstone/src/main/java/com/google/univiz/CollegeDataApi.java
+++ capstone/src/main/java/com/google/univiz/CollegeDataApi.java
@@ -5,5 +5,5 @@ import java.util.Set;
 
 public interface CollegeDataApi {
 
-  List<CollegeData> getCollegesById(Set<String> ids, Set<String> names);
+  List<CollegeData> getCollegesById(Set<String> ids);
 }
