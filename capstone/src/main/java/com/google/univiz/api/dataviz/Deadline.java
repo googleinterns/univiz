ed86c2446b01d58bd2f8b99086d0c0d26593c0d7
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-07 11:07:44 -0700

    Add Javadoc to com.google.univiz.api.dataviz classes

--- capstone/src/main/java/com/google/univiz/api/dataviz/Deadline.java
+++ capstone/src/main/java/com/google/univiz/api/dataviz/Deadline.java
@@ -1,3 +1,7 @@
 package com.google.univiz.api.dataviz;
 
+/**
+ * Contains the date when applications are due for a particular college.
+ * Will be made serializable for the purpose of graphing due dates.
+ */
 public class Deadline {}
--- capstone/src/main/java/com/google/univiz/api/dataviz/TuitionStats.java
+++ capstone/src/main/java/com/google/univiz/api/dataviz/TuitionStats.java
@@ -1,3 +1,8 @@
 package com.google.univiz.api.dataviz;
 
+/**
+ * Contains statistics related to tuition for a particular
+ * college. The following will be included: average tuition cost before and
+ * after financial aid is considered, and tuition cost over the last five years.
+ */
 public class TuitionStats {}
