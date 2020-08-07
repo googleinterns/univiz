fa090f6c71073459668c7c5a5e8fa42761430417
Bianca Macias <bmac@biancamacias.com>
2020-08-07 01:07:54 +0000

    added enum elements to CarnegieSizeDegree class and UrbanizationDegree class

---
+++ capstone/src/main/java/com/google/univiz/UrbanizationDegree.java
@@ -0,0 +1,21 @@
+package com.google.univiz;
+
+/**
+ * UrbanizationDegree is a class that holds the enum for the range of values for a school's
+ * urbanization degree. The index of each of these values is the urbanization degree provided by
+ * ScorecardData.
+ */
+public class UrbanizationDegree {
+
+  public enum UrbanizationDegree {
+    NOT_AVAILABLE("Degree Unavailable"),
+    LARGE_CITY("Large city"),
+    MIDSIZE_CITY("Midsize city"),
+    URBAN_LARGE_CITY("Urban large city"),
+    URBAN_MIDSIZE_CITY("Urban midsize city"),
+    LARGE_TOWN("Large town"),
+    SMALL_TOWN("Small town"),
+    RURAL("Rural"),
+    RURAL_MSA("Rural");
+  }
+}
