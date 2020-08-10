86894a684cea6f27a550fdaaf86c9b068ddc9a24
mmurray22 <michaelamurray2357@gmail.com>
2020-08-10 21:40:09 +0000

    Changes to test file

--- capstone/src/main/webapp/js/autocompleteTest.js
+++ capstone/src/main/webapp/js/autocompleteTest.js
@@ -1,4 +1,4 @@
-describe('Autocomplete Suggests correct array', () => {
+describe('Autocomplete Suggests correct array', function() {
   const data = ['Hello', 'Hi', 'Howdy', 'Yello'];
   document.getElementById('search').value = 'H';
   let autoItems = document.getElementsByClassName('autocomplete-items');
