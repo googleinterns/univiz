a80187b55e9f762f3f8cbe2c20dfd99b93a2c39c
Bianca Macias <bmac@biancamacias.com>
2020-08-10 19:12:14 +0000

    fixed interface nits

--- capstone/src/main/java/com/google/univiz/CollegeDataApi.java
+++ capstone/src/main/java/com/google/univiz/CollegeDataApi.java
@@ -1,13 +1,11 @@
 package com.google.univiz;
 
 import java.util.List;
-import java.util.Set;
 
 /**
- * CollegeDataApi is an interface for the CollegeDataApiImpl class that interacts with external APIs
- * to send REST requests.
+ * Provides access to college related data.
  */
 public interface CollegeDataApi {
 
-  List<CollegeData> getCollegesById(Set<CollegeId> ids);
+  List<CollegeData> getCollegesById(List<CollegeId> ids);
 }
