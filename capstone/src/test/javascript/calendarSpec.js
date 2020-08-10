8e7c1f6bd747e01dc4fc3a79260913d2bacaa6bc
Chris Condit <ccondit@google.com>
2020-08-10 16:56:55 -0700

    Switches to chrome headless

--- capstone/src/test/javascript/calendarSpec.js
+++ capstone/src/test/javascript/calendarSpec.js
@@ -1,21 +1,15 @@
-describe('DataTable Construction 2', function() {
-  it('Will add correct columns/rows to DataTable from JSON', function() {
-    const mockCalenderInfo = {};
-    const expectedFreqData = {};
-    const expectedTimelineData = {};
-    const actualFreqData = {};
-    const actualTimelineData = {};
-    window.initMap = function() {
-      // TODO(ihsan314): adjust table and mock calendar info
-      // when format of JSON is confirmed
-      dum
-      expectedFreqData = new google.visualization.DataTable();
-      expectedTimelineData = new google.visualization.DataTable();
-      actualFreqData = new google.visualization.DataTable();
-      actualTimelineData = new google.visualization.DataTable();
-      populateDataTables(actualFreqData, actualTimelineData, mockCalenderInfo);
+describe('Data Table Construction', () => {
+  it('Will add correct columns/rows to DataTable from JSON', () => {
+    let expectedData = {};
+    let actualData = {};
+    const mockTuitionInfo = {};
+    window.initTuitionData = () => {
+      // TODO(ihsan314): Initialize this when format of
+      // JSON object is well-defined
+      expectedData = new google.visualization.DataTable();
+      actualData = new google.visualization.DataTable();
+      populateDataTable(actualData, mockTuitionInfo);
     };
-    expect(actualFreqData).toEqual(expectedFreqData);
-    expect(actualTimelineData).toEqual(expectedTimelineData);
+    expect(actualData).toEqual(expectedData);
   });
 });
