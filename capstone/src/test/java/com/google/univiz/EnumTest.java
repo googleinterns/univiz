7bf5d85fcdb65d4d0ef56b118e71d8750e5e9b93
Bianca Macias <bmac@biancamacias.com>
2020-08-10 17:41:54 +0000

    formatting nits

--- capstone/src/test/java/com/google/univiz/EnumTest.java
+++ capstone/src/test/java/com/google/univiz/EnumTest.java
@@ -9,10 +9,12 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public final class EnumTest {
 
-    @Test
-    public void testIndexing() {
-        assertThat(CarnegieSizeDegree.getDegree(0)).isEqualTo("Degree unavailable");
-        assertThat(CarnegieSizeDegree.getDegree(8)).isEqualTo("Four-year college, very small and residential");
-        assertThat(CarnegieSizeDegree.getDegree(15)).isEqualTo("Four-year college, large and mostly nonresidential");
-    }
+  @Test
+  public void testIndexing() {
+    assertThat(CarnegieSizeDegree.getDegree(0)).isEqualTo("Degree unavailable");
+    assertThat(CarnegieSizeDegree.getDegree(8))
+        .isEqualTo("Four-year college, very small and residential");
+    assertThat(CarnegieSizeDegree.getDegree(15))
+        .isEqualTo("Four-year college, large and mostly nonresidential");
+  }
 }
