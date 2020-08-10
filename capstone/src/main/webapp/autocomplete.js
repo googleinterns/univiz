5ca411a226c130deaacea3249fdde4cde4e4aaf5
mmurray22 <michaelamurray2357@gmail.com>
2020-08-10 16:32:13 +0000

    Changes style in autocomplete

--- capstone/src/main/webapp/autocomplete.js
+++ capstone/src/main/webapp/autocomplete.js
@@ -10,50 +10,56 @@ function autocomplete(inp, arr) {
   let currentFocus;
   inp.addEventListener('input', function(e) {
     closeAllLists();
-    let val = this.value;
+    const val = this.value;
     if (!val) { 
       return false;
     }
     currentFocus = -1;
-    let a = document.createElement('DIV');
-    a.setAttribute('id', this.id + 'autocomplete-list');
-    a.setAttribute('class', 'autocomplete-items');
-    this.parentNode.appendChild(a);
+
+    const autocompleteList = document.createElement('DIV');
+    autocompleteList.setAttribute('id', this.id + 'autocomplete-list');
+    autcompleteList.setAttribute('class', 'autocomplete-items');
+    this.parentNode.appendChild(autocompleteList);
+    
+
     for (let i = 0; i < arr.length; i++) {
       if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-        let b = document.createElement('DIV');
-        b.innerHTML = '<strong>' + arr[i].substr(0, val.length) + '</strong>';
-        b.innerHTML += arr[i].substr(val.length);
-        b.innerHTML += '<input type='+'hidden'+'value='+arr[i]+'>';
-        b.addEventListener('click', function(e) {
+        
+	const listElement = document.createElement('DIV');
+        listElement.innerHTML = '<strong>' + arr[i].substr(0, val.length) + '</strong>';
+        listElement.innerHTML += arr[i].substr(val.length);
+        listElement.innerHTML += '<input type='+'hidden'+'value='+arr[i]+'>';
+        listElement.addEventListener('click', function(e) {
           inp.value = this.getElementsByTagName('input')[0].value;
           closeAllLists();
         });
-        a.appendChild(b);
+        
+	autocompleteList.appendChild(listElement);
       }
     }
   });
 
   inp.addEventListener('keydown', function(e) {
-    let x = document.getElementById(this.id + 'autocomplete-list');
-    if (x) {
-      x = x.getElementsByTagName('div');
+    let autocompleteListElmt = document.getElementById(this.id + 'autocomplete-list');
+    if (autocompleteListElmt) {
+      autocompleteListElmt = autocompleteListElmt.getElementsByTagName('div');
     }
     if (e.keyCode == 40) {
       currentFocus++;
-      addActive(x);
+      addActive(autocompleteListElmt);
     } else if (e.keyCode == 38) { 
       currentFocus--;
-      addActive(x);
+      addActive(autocompleteListElmt);
     } else if (e.keyCode == 13) {
       e.preventDefault();
       if (currentFocus > -1) {
-        if (x) {
-          x[currentFocus].click();
+        if (autocompleteListElmt) {
+          autocompleteListElmt[currentFocus].click();
         }
       }
     }
   });
+
   function addActive(x) {
     if (!x) {
       return false;
@@ -67,11 +73,13 @@ function autocomplete(inp, arr) {
     }
     x[currentFocus].classList.add('autocomplete-active');
   }
+
   function removeActive(x) {
     for (let i = 0; i < x.length; i++) {
       x[i].classList.remove('autocomplete-active');
     }
   }
+
   function closeAllLists(elmnt) {
     const autocomplete_items = document.getElementsByClassName('autocomplete-items');
     for (let i = 0; i < autocomplete_items.length; i++) {
@@ -80,6 +88,7 @@ function autocomplete(inp, arr) {
       }
     }
   }
+
   document.addEventListener('click', function(e){
     closeAllLists(e.target);
   });
