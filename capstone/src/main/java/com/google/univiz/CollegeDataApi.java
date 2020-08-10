23e5eb2ef83e36b14e2a961efeb2240a6d68b229
Bianca Macias <bmac@biancamacias.com>
2020-08-10 19:14:42 +0000

    formatting nits

--- capstone/src/main/java/com/google/univiz/CollegeDataApi.java
+++ capstone/src/main/java/com/google/univiz/CollegeDataApi.java
@@ -2,9 +2,7 @@ package com.google.univiz;
 
 import java.util.List;
 
-/**
- * Provides access to college related data.
- */
+/** Provides access to college related data. */
 public interface CollegeDataApi {
 
   List<CollegeData> getCollegesById(List<CollegeId> ids);
