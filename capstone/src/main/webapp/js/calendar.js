5381274d54135c10ce116067ffdfd5cd6dc83289
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-10 16:09:14 -0700

    Restrict lines to length of 80 characters

--- capstone/src/main/webapp/js/calendar.js
+++ capstone/src/main/webapp/js/calendar.js
@@ -33,8 +33,8 @@ function drawFrequencyChart(freqData) {
 
 /**
  * Generate suggested timeline of actions related to deadlines.
- * @param {google.visualization.DataTable} timelineData the DataTable corresponding
- *     to the suggested actions to take before the deadlines
+ * @param {google.visualization.DataTable} timelineData the DataTable
+ *     corresponding to the suggested actions to take before the deadlines
  */
 function drawTimelineChart(timelineData) {
   const options = {
@@ -46,16 +46,16 @@ function drawTimelineChart(timelineData) {
 }
 
 /**
- * Helper function to transfer elements from JSON object with deadline information
- * to two DataTables.
+ * Helper function to transfer elements from JSON object with deadline
+ * information to two DataTables.
  *
- * @param {google.visualization.DataTable} freqData the DataTable with the data the
- *     frequency chart will use. Will consist of sample deadlines for each of the user's
- *     selected colleges.
+ * @param {google.visualization.DataTable} freqData the DataTable with the data
+ *     the frequency chart will use. Will consist of sample deadlines for each
+ *     of the user's selected colleges.
  *
  * @param {google.visualization.DataTable} timelineData * the DataTable with the
- *     data for the timeline chart. Will consist of suggested courses of action for
- *     the user based on sample deadlines.
+ *     data for the timeline chart. Will consist of suggested courses of action
+ *     for the user based on sample deadlines.
  *
  * @param {JSON} deadlineInfo the JSON object with the deadline and timeline
  *     information that will be added to the previously mentioned DataTables.
