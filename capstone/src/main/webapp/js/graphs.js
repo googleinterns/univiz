fea87de028d84da95349aabfc02d2cba77ea5103
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-10 12:41:40 -0700

    Switch to arrow function

--- capstone/src/main/webapp/js/graphs.js
+++ capstone/src/main/webapp/js/graphs.js
@@ -2,7 +2,7 @@ const script = document.createElement('script');
 script.src = 'https://www.gstatic.com/charts/loader.js';
 script.defer = true;
 
-window.initTuitionData = function() {
+window.initTuitionData = () -> {
   google.charts.load('current', {packages: ['corechart', 'bar']});
   google.charts.setOnLoadCallback(graphGrossNetTuition);
 
--- capstone/src/test/javascript/graphsSpec.js
+++ capstone/src/test/javascript/graphsSpec.js
@@ -1,9 +1,9 @@
-describe('Data Table Construction', function() {
-  it('Will add correct columns/rows to DataTable from JSON', function() {
+describe('Data Table Construction', () -> {
+  it('Will add correct columns/rows to DataTable from JSON', () -> {
     const expectedData;
     const actualData;
     const mockTuitionInfo = {};
-    window.initTuitionData = function() {
+    window.initTuitionData = () -> {
       // TODO(ihsan314): Initialize this when format of
       // JSON object is well-defined
       expectedData = new google.visualization.DataTable();
