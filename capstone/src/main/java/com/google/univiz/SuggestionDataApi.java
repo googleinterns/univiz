4e554fdbfafbc62c45f9ddc4a352b76b13ab1014
mmurray22 <michaelamurray2357@gmail.com>
2020-08-11 20:47:50 +0000

    Updates formatting

--- capstone/src/main/java/com/google/univiz/SuggestionDataApi.java
+++ capstone/src/main/java/com/google/univiz/SuggestionDataApi.java
@@ -3,13 +3,13 @@ package com.google.univiz;
 import java.util.List;
 
 /**
- * Communicates directly with the data source to get a list of college suggestions related to a client
- * query
+ * Communicates directly with the data source to get a list of college suggestions related to a
+ * client query
  */
 public interface SuggestionDataApi {
   /**
-   * Takes a college name and returns a list of SuggestionData objects, where each object
-   * is unique to each college name provided in the first list
+   * Takes a college name and returns a list of SuggestionData objects, where each object is unique
+   * to each college name provided in the first list
    */
   List<SuggestionData> getCollegeSuggestions(String collegeName);
 }
