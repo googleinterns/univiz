f3d450041bc44c0636fc8f24c6447ea5febc47d8
mmurray22 <michaelamurray2357@gmail.com>
2020-08-11 22:44:43 +0000

    Formats java file

--- capstone/src/main/java/com/google/univiz/api/SearchResource.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResource.java
@@ -12,10 +12,10 @@ public interface SearchResource {
    * query.
    *
    * @param query The search query as the user provided. It should be a non-empty and a non-null
-   *     string. 
-   * @return A list of possible matching colleges. The list is ordered such that the best match is 
-   *     at the beginning of the list. The list may be empty which indicates there are no possible 
-   *     matches. The list is also not an exhaustive set of matches. It only includes few top 
+   *     string.
+   * @return A list of possible matching colleges. The list is ordered such that the best match is
+   *     at the beginning of the list. The list may be empty which indicates there are no possible
+   *     matches. The list is also not an exhaustive set of matches. It only includes few top
    *     results. If the string is invalid (empty, null, etc.), an empty list will be returned
    */
   List<SearchData> getSearchSuggestions(String collegeNames);
