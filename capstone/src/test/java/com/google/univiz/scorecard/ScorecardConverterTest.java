84be8e7653f6150129512ef8ad19692f25c884ee
Bianca Macias <bmac@biancamacias.com>
2020-08-11 23:11:21 +0000

    make field constants accesible when testing and asserting in ScorecardConverterTest

--- capstone/src/test/java/com/google/univiz/scorecard/ScorecardConverterTest.java
+++ capstone/src/test/java/com/google/univiz/scorecard/ScorecardConverterTest.java
@@ -12,24 +12,38 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public final class ScorecardConverterTest {
 
+  private static final int COLLEGE_ID = 193900;
+  private static final String NAME = "New York University";
+  private static final String CITY = "New York";
+  private static final int FLAG_MAIN_CAMPUS = 1;
+  private static final double LATITUDE = 40.729452;
+  private static final double LONGITUDE = -73.997264;
+  private static final int CARNEGIE_SIZE_DEGREE = 17;
+  private static final double ADMISSION_RATE = 0.1999;
+  private static final double AVG_SAT = 1419.0;
+  private static final int NUM_OF_UNDERGRADS = 26339;
+  private static final int AVG_COST = 69830;
+  private static final double RATIO_OF_MEN = 0.4253;
+  private static final double RATIO_OF_WOMEN = 0.5747;
+
   @Test
   public void converterTest() throws Exception {
     ScorecardData.Builder scorecardBuilder = ScorecardData.builder();
     ScorecardData scorecardData =
         scorecardBuilder
-            .setId(193900)
-            .setName("New York University")
-            .setCity("New York")
-            .setFlagMainCampus(1)
-            .setLatitude(40.729452)
-            .setLongitude(-73.997264)
-            .setCarnegieSizeDegree(17)
-            .setAdmissionRate(0.1999)
-            .setAvgSat(1419.0)
-            .setNumOfUndergrads(26339)
-            .setAvgCost(69830)
-            .setRatioOfMen(0.4253)
-            .setRatioOfWomen(0.5747)
+            .setId(COLLEGE_ID)
+            .setName(NAME)
+            .setCity(CITY)
+            .setFlagMainCampus(FLAG_MAIN_CAMPUS)
+            .setLatitude(LATITUDE)
+            .setLongitude(LONGITUDE)
+            .setCarnegieSizeDegree(CARNEGIE_SIZE_DEGREE)
+            .setAdmissionRate(ADMISSION_RATE)
+            .setAvgSat(AVG_SAT)
+            .setNumOfUndergrads(NUM_OF_UNDERGRADS)
+            .setAvgCost(AVG_COST)
+            .setRatioOfMen(RATIO_OF_MEN)
+            .setRatioOfWomen(RATIO_OF_WOMEN)
             .build();
 
     Converter<ScorecardData, CollegeData> converter = new ScorecardConverter();
