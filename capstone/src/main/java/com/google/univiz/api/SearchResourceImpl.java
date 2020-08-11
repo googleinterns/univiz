d6d49cb296376404b7bfbc31c60b6511fcda376f
mmurray22 <michaelamurray2357@gmail.com>
2020-08-11 16:26:16 +0000

    Adds first Guice injection

--- capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
@@ -1,15 +1,21 @@
 package com.google.univiz.api;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 
 class SearchResourceImpl implements SearchResource {
+  private List<SuggestionData> collegeSuggestions;
+
+  @Inject
+  public void Restaurant(collegeSuggestions) {
+    this.collegeSuggestions = collegeSuggestions;
+  }
 
   @Override
   public List<SearchData> getSearchSuggestions(String partialCollegeName) {
     List<SearchData> searchDataList = new ArrayList<>();
-    List<SuggestionData> collegeSuggestions = getSuggestions(partialCollegeName); // TODO
-    for (SuggestionData college : collegeSuggestions) {
+    for (SuggestionData college : this.collegeSuggestions) {
       SearchData searchDataObj =
           SearchData.create(college.getCollegeName(), college.getCollegeId());
       searchDataList.add(searchDataObj);
