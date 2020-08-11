eea3199878e6974b71f5fbdc2b17da0d26ccaeac
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-08-11 15:23:55 -0700

    Updates javadoc

--- capstone/src/main/java/com/google/univiz/api/SearchResource.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResource.java
@@ -11,12 +11,12 @@ public interface SearchResource {
    * Given a potentially incomplete search query, returns a list of colleges that would match the
    * query.
    *
-   * @param query The search query as the user typed in. It should be a non-empty and a non-null
-   *     string.
+   * @param query The search query as the user provided. It should be a non-empty and a non-null
+   *     string. 
    * @return A list of possible matching colleges. The list is ordered such that the best
    *     match is at the beginning of the list. The list may be empty which indicates there are no
    *     possible matches. The list is also not an exhaustive set of matches. It only includes few
-   *     top results.
+   *     top results. If the string is invalid (empty, null, etc.), an empty list will be returned
    */
   List<SearchData> getSearchSuggestions(String collegeNames);
 }
