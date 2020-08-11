003da3185326b1ac11309f51019c1490f01672f9
mmurray22 <michaelamurray2357@gmail.com>
2020-08-11 22:41:10 +0000

    Formats test file

--- capstone/src/test/java/com/google/univiz/api/SearchResourceImplTest.java
+++ capstone/src/test/java/com/google/univiz/api/SearchResourceImplTest.java
@@ -1,10 +1,5 @@
 package com.google.univiz.api;
 
-import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
-import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,12 +7,10 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class SearchResourceImplTest {
-  @Mock
-  SuggestionData mockSuggestionData;
+  @Mock SuggestionData mockSuggestionData;
 
   @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
   @Test
-  public void testGetSuggestions() throws Exception {
-  }	  
+  public void testGetSuggestions() throws Exception {}
 }
