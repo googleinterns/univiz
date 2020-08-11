2760b29648386748e37cda572ff1e72b3a265d1b
mmurray22 <michaelamurray2357@gmail.com>
2020-08-11 23:11:23 +0000

    Changes testing

--- capstone/src/main/webapp/js/autocompleteTest.js
+++ capstone/src/main/webapp/js/autocompleteTest.js
@@ -1,12 +1,14 @@
 describe('Autocomplete Suggests correct array', function() {
-  const data = ['Allo!', 'Hello', 'Hi', 'Howdy', 'Yello'];
-  autocomplete(data);
-  const autoItems = document.getElementsByClassName('autocomplete-items');
-  const i = 0;
-  for (item in autoItems) {
-    if (true) {
-      expect(item.innerHTML).toBe(data[i]);
-      i++;
+  $(document).ready(function() {
+    const data = ['Allo!', 'Hello', 'Hi', 'Howdy', 'Yello'];
+    autocomplete(data);
+    const autoItems = document.getElementsByClassName('autocomplete-items');
+    const i = 0;
+    for (item in autoItems) {
+      if (true) {
+        expect(item.innerHTML).toBe(data[i]);
+        i++;
+      }
     }
-  }
+  });
 });
