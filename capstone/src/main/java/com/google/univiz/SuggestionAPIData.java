a78420cdd7becb1bada67e488647a3df7407f77d
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-08-10 20:45:56 -0700

    Update capstone/src/main/java/com/google/univiz/SuggestionAPIData.java
    Co-authored-by: Bianca Macias <51244558+biancamacias@users.noreply.github.com>

--- capstone/src/main/java/com/google/univiz/SuggestionAPIData.java
+++ capstone/src/main/java/com/google/univiz/SuggestionAPIData.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Communicates directly with the REST API to get a list of college suggestions related to a client
  * query
  */
-public interface SuggestionDataAPI {
+public interface SuggestionDataApi {
   /**
    * Takes a list of college names and returns a list of SuggestionData objects, where each object
    * is unique to each college name providede in the first list
