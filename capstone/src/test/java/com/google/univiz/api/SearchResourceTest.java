bb3c6c1bd849361dd55e528690002d7186b79ee5
mmurray22 <michaelamurray2357@gmail.com>
2020-08-11 05:01:18 +0000

    Adds test files

---
+++ capstone/src/test/java/com/google/univiz/api/SearchResourceTest.java
@@ -0,0 +1,13 @@
+package com.google.univiz.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class SearchResourceTest {
+  @Test
+  public void testSearchSuggestions() {
+    /**/
+  }
+}
