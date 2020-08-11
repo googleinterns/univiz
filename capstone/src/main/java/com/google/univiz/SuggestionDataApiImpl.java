6c14f443a2695e095e888aba3e84fa4e320123fb
mmurray22 <michaelamurray2357@gmail.com>
2020-08-11 20:43:56 +0000

    Adds further changes to the impl

--- capstone/src/main/java/com/google/univiz/SuggestionDataApiImpl.java
+++ capstone/src/main/java/com/google/univiz/SuggestionDataApiImpl.java
@@ -5,8 +5,15 @@ import org.json.simple.JSONObject;
 
 public class SuggestionDataApiImpl implements SuggestionDataApi {
   private static final frontUrl = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=";
+  
+  /** TODO: Javadoc */
   private JSONObject requestRESTApiData(String fields, String proposedCollegeName);
-  private List<SuggestionData> convertJsonToSuggestionData(JSONObject suggestionResults);
+  /** TODO: Javadoc */
+  private List<SuggestionData> convertJsonToSuggestionData(JSONObject suggestionResults) {
+  
+  }
+  
+  /** TODO: Javadoc */
   @Override
   public List<SuggestionData> getSuggestions(String collegeName);
 }
