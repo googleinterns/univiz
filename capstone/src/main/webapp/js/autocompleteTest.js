34af6d57204455f4228386e60cb369c56ff14a29
mmurray22 <michaelamurray2357@gmail.com>
2020-08-12 02:23:06 +0000

    Changes more about the test

--- capstone/src/main/webapp/js/autocompleteTest.js
+++ capstone/src/main/webapp/js/autocompleteTest.js
@@ -1,15 +1,18 @@
 describe('Autocomplete Suggests correct array', function() {
-  let search = null;
+  let search;
+
   beforeEach(function() {
    let id = 'search';
    search = $('<input id='+id+'/>');
    $(document.body).append(search);
   });
-  it('Autocomplete selects the right suggestion items', () => {
+
+  it('Autocomplete selects correct items', () => {
    const fullData = ['Ahoy', 'Hello', 'Wattup'];
    const relevantData = ['Hello'];
    autocomplete(fullData);
   })
+
   afterEach(function(){
     search.remove();
     search = null;
