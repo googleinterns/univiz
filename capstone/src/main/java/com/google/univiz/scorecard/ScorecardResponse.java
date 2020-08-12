5f4a15e4201ca4f3f155e5dc6bc68706669d35f3
Bianca Macias <bmac@biancamacias.com>
2020-08-12 22:23:33 +0000

    added javadoc and added test for ScorecardResponse

--- capstone/src/main/java/com/google/univiz/scorecard/ScorecardResponse.java
+++ capstone/src/main/java/com/google/univiz/scorecard/ScorecardResponse.java
@@ -1,13 +1,19 @@
 package com.google.univiz.scorecard;
 
 import com.google.auto.value.AutoValue;
+import com.google.gson.annotations.SerializedName;
 import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
 import java.util.List;
 
+/**
+ * ScorecardResponse is a class representing a list of ScorecardData types. This class parses the
+ * JSON by only taking the JSON data from the "results", which is the array that holds the
+ * college/university/institution data needed for the UniViz app.
+ */
 @GenerateTypeAdapter
 @AutoValue
 abstract class ScorecardResponse {
-  
-  @SeralizedName("results")
+
+  @SerializedName("results")
   abstract List<ScorecardData> scorecardData();
 }
