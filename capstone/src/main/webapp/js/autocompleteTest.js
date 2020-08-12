3714f3131d946d27a09c1f3a200f2fdeb81bfb4e
mmurray22 <michaelamurray2357@gmail.com>
2020-08-12 01:37:12 +0000

    Updates test

--- capstone/src/main/webapp/js/autocompleteTest.js
+++ capstone/src/main/webapp/js/autocompleteTest.js
@@ -1,7 +1,17 @@
 describe('Autocomplete Suggests correct array', function() {
+  let search = null;
+  beforeEach(function() {
+   let id = 'search';
+   search = $('<input id='+id+'/>');
+   $(document.body).append(search);
+  });
   it('Autocomplete selects the right suggestion items', () => {
    const fullData = ['Ahoy', 'Hello', 'Wattup'];
    const relevantData = ['Hello'];
    autocomplete(fullData);
   })
+  afterEach(function(){
+    search.remove();
+    search = null;
+  });
 });
