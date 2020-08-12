b4698b642759e43d530ec65efff28976ad3b35f9
mmurray22 <michaelamurray2357@gmail.com>
2020-08-12 22:26:34 +0000

    Changes for test

--- capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
@@ -1,19 +1,23 @@
 package com.google.univiz.api;
 
+//TODO: imports
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class SearchResourceImpl implements SearchResource {
+  private final SuggestionApi suggestionApi;
+  
+  @Inject
+  SearchResourceImpl(SuggestionApi suggestionApi) {
+    this.suggestionApi =  suggestionApi;
+  }
 
   @Override
   public static List<SearchData> getSearchSuggestions(String partialCollegeName) {
-    List<SearchData> searchDataList = new ArrayList<>();
-    List<SuggestionData> collegeSuggestions = getSuggestions(partialCollegeName); // TODO
-    for (SuggestionData college : collegeSuggestions) {
-      SearchData searchDataObj =
-          SearchData.create(college.getCollegeName(), college.getCollegeId());
-      searchDataList.add(searchDataObj);
-    }
-    return searchDataList;
+    List<SuggestionData> collegeSuggestions = suggestionApi.getSuggestions(partialCollegeName);
+    return collegeSuggestions.stream()
+	                     .map(college -> SearchData.create(college.getCollegeName(), college.getCollegeId()))
+	                     .collect(Collectors.toList());
   }
 }
--- capstone/src/test/java/com/google/univiz/api/SearchResourceImplTest.java
+++ capstone/src/test/java/com/google/univiz/api/SearchResourceImplTest.java
@@ -1,5 +1,9 @@
 package com.google.univiz.api;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +11,7 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class SearchResourceImplTest {
-  @Mock SuggestionData mockSuggestionData;
+  @Mock private SuggestionApi mockSuggestionApi; 
 
   @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
