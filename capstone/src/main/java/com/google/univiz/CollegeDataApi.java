0018c064ea7d188e1969e66d1640d5d4f18285a8
Bianca Macias <51244558+biancamacias@users.noreply.github.com>
2020-08-12 12:05:42 -0700

    CollegeData.java class  (#66)
    CollegeData class, ScorecardConverter class, CollegeId class, CarnegieSizeDegree enum class, tests for all of these classes.

---
+++ capstone/src/main/java/com/google/univiz/CollegeDataApi.java
@@ -0,0 +1,9 @@
+package com.google.univiz;
+
+import java.util.List;
+
+/** Provides access to college related data. */
+public interface CollegeDataApi {
+
+  List<CollegeData> getCollegesById(List<CollegeId> ids);
+}
