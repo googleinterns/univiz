8a4a420509c42de7ca6e499c717fb424b6aeaf2e
mmurray22 <michaelamurray2357@gmail.com>
2020-08-12 23:04:03 +0000

    Fixes Java testing formatting

--- capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
+++ capstone/src/main/java/com/google/univiz/api/SearchResourceImpl.java
@@ -1,25 +1,24 @@
 package com.google.univiz.api;
 
-//TODO: imports
+// TODO: imports
 //
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 
 class SearchResourceImpl implements SearchResource {
   private final SuggestionDataApi suggestionApi;
-  
+
   @Inject
   SearchResourceImpl(SuggestionDataApi suggestionApi) {
-    this.suggestionApi =  suggestionApi;
+    this.suggestionApi = suggestionApi;
   }
 
   @Override
   public List<SearchData> getSearchSuggestions(String partialCollegeName) {
     List<SuggestionData> collegeSuggestions = suggestionApi.getSuggestions(partialCollegeName);
     return collegeSuggestions.stream()
-	                     .map(college -> SearchData.create(college.collegeName(), college.collegeId()))
-	                     .collect(Collectors.toList());
+        .map(college -> SearchData.create(college.collegeName(), college.collegeId()))
+        .collect(Collectors.toList());
   }
 }
