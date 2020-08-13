d92736b654fcbb55d9fae6c0b7522e5f9cb47292
mmurray22 <michaelamurray2357@gmail.com>
2020-08-13 22:43:10 +0000

    Formats test

--- capstone/src/test/java/com/google/univiz/SuggestionDataTest.java
+++ capstone/src/test/java/com/google/univiz/SuggestionDataTest.java
@@ -6,24 +6,20 @@ import com.google.common.io.Resources;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-
 @RunWith(JUnit4.class)
 public final class SuggestionDataTest {
-  /**
-   * Tests whether the JSON properly deserializes into a SuggestionData object
-   */
+  /** Tests whether the JSON properly deserializes into a SuggestionData object */
   @Test
   public void testJsonDeserializes() throws Exception {
     Gson gson = new GsonBuilder().registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY).create();
     InputStreamReader suggestionDataReader =
         new InputStreamReader(
-            Resources.getResource(SuggestionDataTest.class, "suggestion.json").openStream()); 
+            Resources.getResource(SuggestionDataTest.class, "suggestion.json").openStream());
     SuggestionData suggestionData = gson.fromJson(suggestionDataReader, SuggestionData.class);
 
     assertThat(suggestionData.name()).isEqualTo("Stanford University");
