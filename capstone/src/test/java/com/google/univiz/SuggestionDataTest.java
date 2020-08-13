253c385632ee8d2f20d570764993816cffc13cd3
mmurray22 <michaelamurray2357@gmail.com>
2020-08-13 22:28:09 +0000

    Testing the SuggestionData class now-currently not passing

---
+++ capstone/src/test/java/com/google/univiz/SuggestionDataTest.java
@@ -0,0 +1,32 @@
+package com.google.univiz;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+@RunWith(JUnit4.class)
+public final class SuggestionDataTest {
+  /**
+   * Tests whether the JSON properly deserializes into a SuggestionData object
+   */
+  @Test
+  public void testJsonDeserializes() throws Exception {
+    Gson gson = new GsonBuilder().registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY).create();
+    InputStream suggestionDataStream = this.getClass().getClassLoader().getResourceAsStream("suggestion.json");
+    InputStreamReader suggestionDataReader =
+        new InputStreamReader(suggestionDataStream);
+    SuggestionData suggestionData = gson.fromJson(suggestionDataReader, SuggestionData.class);
+
+    assertThat(suggestionData.name()).isEqualTo("Stanford University");
+    assertThat(suggestionData.id()).isEqualTo(243744);
+  }
+}
