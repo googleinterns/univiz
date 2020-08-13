f6458886f064cda686c7298b5d352485cc2e6309
mmurray22 <michaelamurray2357@gmail.com>
2020-08-13 23:21:41 +0000

    Adds test file

---
+++ capstone/src/test/java/com/google/univiz/SuggestionApiImplTest.java
@@ -0,0 +1,19 @@
+package com.google.univiz;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
+import java.io.InputStreamReader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class SuggestionDataApiImplTest {
+  @Test
+  public void testGetSuggestions() {
+  }
+}
