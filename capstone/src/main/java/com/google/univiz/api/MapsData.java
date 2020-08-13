d10310226e43de42c44b649a8012ba3fb4358ea3
Bianca Macias <bmac@biancamacias.com>
2020-08-13 17:36:52 +0000

    renamed MapsResource and made MapsData Autovalue

--- capstone/src/main/java/com/google/univiz/api/MapsData.java
+++ capstone/src/main/java/com/google/univiz/api/MapsData.java
@@ -1,4 +1,41 @@
 package com.google.univiz.api;
 
-/** The MapsData class will contain all college data that is specific to the Maps feature */
-public class MapsData {}
+import com.google.auto.value.AutoValue;
+
+/**
+ * The MapsData is a class representing all relevant college data that is specific to the Maps
+ * feature
+ */
+@AutoValue
+public abstract class MapsData {
+
+  public abstract String name();
+
+  public abstract String city();
+
+  public abstract boolean isMainCampus();
+
+  public abstract double latitude();
+
+  public abstract double longitude();
+
+  public static Builder builder() {
+    return new AutoValue_MapData.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    
+    public abstract Builder setName(String value);
+
+    public abstract Builder setCity(String city);
+
+    public abstract Builder setIsMainCampus(boolean value);
+
+    public abstract Builder setLatitude(double value);
+
+    public abstract Builder setLongitude(double value);
+
+    public abstract MapsData build();
+  }
+}
--- capstone/src/main/java/com/google/univiz/api/MapsResource.java
+++ capstone/src/main/java/com/google/univiz/api/MapsResource.java
@@ -6,10 +6,10 @@ import java.util.List;
  * The MapResource interface is responsible for all data management/processing specific for the maps
  * feature of the UniViz webapp.
  */
-public interface MapResource {
+public interface MapsResource {
   /**
    * Takes a list of college ids and returns a list of MapsData objects to the caller. This object
    * contains all data relevant to the Maps feature.
    */
-  List<MapsData> getMapData(List<CollegeId> colleges);
+  List<MapsData> getMapData(List<CollegeData> colleges);
 }
