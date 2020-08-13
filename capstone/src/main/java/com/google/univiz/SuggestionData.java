99cf7493db8c6b0df96e73f4a21fd515e84f30e7
mmurray22 <michaelamurray2357@gmail.com>
2020-08-13 20:56:06 +0000

    Adds AutoValue to SuggestionData

--- capstone/src/main/java/com/google/univiz/SuggestionData.java
+++ capstone/src/main/java/com/google/univiz/SuggestionData.java
@@ -1,4 +1,19 @@
 package com.google.univiz;
 
+import com.google.auto.value.AutoValue;
+import com.google.gson.annotations.SerializedName;
+import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
+import javax.annotation.Nullable;
+
 /** SuggestionData represents the college data specific to the Search feature */
-public class SuggestionData {}
+@GenerateTypeAdapter
+@AutoValue
+abstract SuggestionData {
+  @SerializedName("school.name")
+  @Nullable
+  abstract String name();
+
+  @SerializedName("id")
+  @Nullable
+  abstract CollegeId id();
+}
