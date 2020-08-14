d1b2cd384ece31a0a991e38a91e9e6afbcc455c7
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-14 15:01:51 -0700

    Make tests compile

--- capstone/src/test/java/com/google/univiz/scorecard/CollegeDataApiImplTest.java
+++ capstone/src/test/java/com/google/univiz/scorecard/CollegeDataApiImplTest.java
@@ -1,17 +1,28 @@
 package com.google.univiz.scorecard;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.when;
 
 import com.google.common.io.Resources;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.inject.Guice;
-import com.google.inject.testing.BoundFieldModule;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.univiz.CollegeData;
+import com.google.univiz.api.CollegeId;
+import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import javax.inject.Inject;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -21,7 +32,6 @@ public final class CollegeDataApiImplTest {
   @Rule public MockitoRule rule = MockitoJUnit.rule();
   @Bind @Mock CollegeIdReaderProvider mockReaderProvider;
   @Bind ScorecardConverter scorecardConverter = new ScorecardConverter();
-  @Bind UnivizConfig config;
   @Inject CollegeDataApiImpl testImpl;
   
   @Before
@@ -31,13 +41,13 @@ public final class CollegeDataApiImplTest {
 
   @Test
   public void testGetCollegesById() throws IOException {
-    Gson gson = GsonBuilder().registerTypeAdapterFactoru(GenerateTypeAdapter.FACTORY).create();
+    Gson gson = new GsonBuilder().registerTypeAdapterFactory(GenerateTypeAdapter.FACTORY).create();
     InputStreamReader scorecardReader = new InputStreamReader(Resources.getResource(CollegeDataApiImplTest.class, "scorecard_response.json").openStream());
     CollegeId collegeId = CollegeId.create(193900);
     when(mockReaderProvider.getReaderFromCollegeId(collegeId)).thenReturn(scorecardReader);
-    List<CollegeData> colleges = testImpl.getCollegesById();
+    List<CollegeData> colleges = testImpl.getCollegesById(Arrays.asList(collegeId));
     assertThat(colleges).hasSize(1);
     CollegeData collegeData = colleges.get(0);
-    assertThat(college.name()).isEqualTo("New York University");
+    assertThat(collegeData.name()).isEqualTo("New York University");
   }
 }
