4d2b04a305a5b083255e83589518d56721e44dbc
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-13 17:57:06 -0700

    Skeleton of test

---
+++ capstone/src/test/java/com/google/univiz/scorecard/CollegeDataApiImplTest.java
@@ -0,0 +1,23 @@
+package com.google.univiz.scorecard;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.io.Resources;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+@RunWith(JUnit4.class)
+public class CollegeDataApiImplTest {
+
+  /**
+  */
+  @Test
+  public void testGetCollegesById() throws IOException {
+  }
+}
