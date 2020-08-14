58f1e3e74cc6670767648330c380a97ead50bcc6
Bianca Macias <bmac@biancamacias.com>
2020-08-14 22:31:25 +0000

    formatting nits

--- capstone/src/test/java/com/google/univiz/api/MapsResourceImplTest.java
+++ capstone/src/test/java/com/google/univiz/api/MapsResourceImplTest.java
@@ -1,26 +1,18 @@
 package com.google.univiz.api;
 
-import static com.google.common.truth.Truth.assertThat;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.Rule;
+import org.juit.runners.JUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.juit.runners.JUnit4;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJunit;
 import org.mockito.junit.MockitoRule;
 
 @RunWith(JUnit4.class)
 public final class MapsResourceImplTest {
 
-    @Mock private CollegeDataApi collegeDataApi;
+  @Mock private CollegeDataApi collegeDataApi;
 
-    @Mock public final MockitoRule rule = MockitoJUnit.rule();
+  @Mock public final MockitoRule rule = MockitoJUnit.rule();
 
-    @Test
-    public void testMapsResourceImpl() {
-
-    }
+  @Test
+  public void testMapsResourceImpl() {}
 }
