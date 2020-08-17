6f27c51cfa719c041341bec76acd5e5fef44794e
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-17 13:16:52 -0700

    Put fields in Set and use StringJoiner to append to URL

--- capstone/src/main/java/com/google/univiz/scorecard/CollegeIdReaderProviderImpl.java
+++ capstone/src/main/java/com/google/univiz/scorecard/CollegeIdReaderProviderImpl.java
@@ -11,6 +11,8 @@ import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 import javax.inject.Inject;
 
 class CollegeIdReaderProviderImpl implements CollegeIdReaderProvider {
@@ -23,17 +25,28 @@ class CollegeIdReaderProviderImpl implements CollegeIdReaderProvider {
 
   @Override
   public Reader getReaderFromCollegeIds(List<CollegeId> ids) throws IOException {
+    Set<String> fields = new HashSet<>();
+    fields.add("id");
+    fields.add("school.name");
+    fields.add("school.city");
+    fields.add("school.main_campus");
+    fields.add("location.lat");
+    fields.add("location.lon");
+    fields.add("school.carnegie_size_setting");
+    fields.add("latest.admissions.admission_rate_overall");
+    fields.add("latest.admissions.sat_scores.average.overall");
+    fields.add("latest.student.size");
+    fields.add("latest.cost.attendance.academic_year");
+    fields.add("latest.student.demographics.men");
+    fields.add("latest.student.demographics.women");
+    String stringWithFields = fields.stream().collect(joining(","));
     final StringBuilder urlStringBuilder = new StringBuilder();
     urlStringBuilder.append("https://api.data.gov/ed/collegescorecard/v1/schools.json?id=");
     String stringWithIds = ids.stream().map(id -> Integer.toString(id.id())).collect(joining(","));
     urlStringBuilder.append(stringWithIds);
-    urlStringBuilder.append("&per_page=1&fields=id,school.name,school.city,school.main_");
-    urlStringBuilder.append("campus,location.lat,location.lon,school.carnegie_size_setting,");
-    urlStringBuilder.append("latest.admissions.admission_rate.overall,");
-    urlStringBuilder.append("latest.admissions.sat_scores.average.overall,latest.student.size,");
-    urlStringBuilder.append(
-        "latest.cost.attendance.academic_year,latest.student.demographics.men,");
-    urlStringBuilder.append("latest.student.demographics.women&api_key=");
+    urlStringBuilder.append("&per_page=1&fields=");
+    urlStringBuilder.append(stringWithFields);
+    urlStringBuilder.append("&api_key=");
     urlStringBuilder.append(univizConfig.scorecardApiKey());
 
     // Make REST call
