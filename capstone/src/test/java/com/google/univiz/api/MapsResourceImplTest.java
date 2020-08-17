f188c9624386cb2bae2e30d7ceec0bd59beb52eb
Bianca Macias <bmac@biancamacias.com>
2020-08-17 18:05:47 +0000

    added setup to test

--- capstone/src/test/java/com/google/univiz/api/MapsResourceImplTest.java
+++ capstone/src/test/java/com/google/univiz/api/MapsResourceImplTest.java
@@ -1,17 +1,33 @@
 package com.google.univiz.api;
 
+import com.google.inject.Guice;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import javax.inject.Inject;
 import org.juit.runners.JUnit4;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 @RunWith(JUnit4.class)
 public final class MapsResourceImplTest {
 
-  @Mock private CollegeDataApi collegeDataApi;
+  @Rule public final MockitoRule rule = MockitoJUnit.rule();
 
-  @Mock public final MockitoRule rule = MockitoJUnit.rule();
+  @Bind @Mock private CollegeDataApi collegeDataApi;
+
+  @Bind private CollegeDataConverter converter = new CollegeDataConverter();
+
+  @Inject private MapsResourceImpl mapsImpl;
+
+  @Before
+  public void guiceSetUp() {
+    Guice.createInjector(BoundFieldModule.of(this)).injectMembers(this);
+  }
 
   @Test
   public void testMapsResourceImpl() {}
