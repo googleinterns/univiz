5e3490c804facf587fe28cb995631cac759b3432
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-08-17 18:03:30 -0700

    Update capstone/src/main/java/com/google/univiz/SuggestionResponse.java
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>

--- capstone/src/main/java/com/google/univiz/SuggestionResponse.java
+++ capstone/src/main/java/com/google/univiz/SuggestionResponse.java
@@ -1,4 +1,4 @@
 package com.google.univiz;
 
-/** SuggestionResponse represents the list college data specific to the Search feature */
+/** SuggestionResponse represents the list of college data specific to the Search feature */
 public class SuggestionResponse {}
