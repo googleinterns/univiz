720661be3097c64e30e4b8233902bc19e9090e1f
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-18 15:29:01 -0700

    Add test for URLProviderImpl

---
+++ capstone/src/test/java/com/google/univiz/scorecard/URLProviderImplTest.java
@@ -0,0 +1,56 @@
+package com.google.univiz.scorecard;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.when;
+
+import com.google.common.io.Resources;
+import com.google.inject.Guice;
+import com.google.inject.testing.fieldbinder.Bind;
+import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.google.univiz.CollegeData;
+import com.google.univiz.api.CollegeId;
+import com.google.univiz.config.UnivizConfigModule;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import javax.inject.Inject;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+@RunWith(JUnit4.class)
+public final class URLProviderImplTest {
+  @Rule public final MockitoRule rule = MockitoJUnit.rule();
+  @Inject private URLProviderImpl testImpl;
+
+  @Before
+  public void setup() {
+    Guice.createInjector(new UnivizConfigModule(), BoundFieldModule.of(this)).injectMembers(this);
+  }
+
+  @Test
+  public void testGetUrlFromCollegeIds() {
+	  CollegeId fakeCollegeId1 = CollegeId.create(1);
+	  CollegeId fakeCollegeId2 = CollegeId.create(2);
+	  CollegeId fakeCollegeId3 = CollegeId.create(3);
+	  List<CollegeId> ids = Arrays.asList(fakeCollegeId1, fakeCollegeId2, fakeCollegeId3);
+	  String actual = testImpl.getUrlFromCollegeIds(ids);
+	  StringBuilder expectedBuilder = new StringBuilder("https://api.data.gov/ed/collegescorecard/v1/schools.json?id=");
+	  expectedBuilder.append(String.format("%d,%d,%d", fakeCollegeId1.id(), fakeCollegeId2.id(), fakeCollegeId3.id()));
+	  expectedBuilder.append("&per_page=1&fields=");
+	  expectedBuilder.append("id,school.name,school.city,school.main_campus,");
+	  expectedBuilder.append("location.lat,location.lon,school.carnegie_size_setting,latest.admissions.admission_rate.overall,");
+	  expectedBuilder.append("latest.admissions.sat_scores.average.overall,latest.student.size,");
+	  expectedBuilder.append("latest.cost.attendance.academic_year,latest.student.demographics.men,");
+	  expectedBuilder.append("latest.student.demographics.women&api_key=");
+	  String expected = expectedBuilder.toString();
+
+	  assertThat(actual.substring(0, expected.length())).isEqualTo(expected);
+  }
+}
