5d69565fd47da5ac146940290d1fedb0d60bd6c4
mmurray22 <michaelamurray2357@gmail.com>
2020-08-18 01:06:16 +0000

    Making changes to match the test with the updates

--- capstone/src/main/java/com/google/univiz/api/representation/SearchData.java
+++ capstone/src/main/java/com/google/univiz/api/representation/SearchData.java
@@ -5,11 +5,11 @@ import com.google.auto.value.AutoValue;
 /** SearchData represents the college data specific to the Search feature */
 @AutoValue
 public abstract class SearchData {
-  static SearchData create(String collegeName, int collegeId) {
+  static SearchData create(String collegeName, CollegeId collegeId) {
     return new AutoValue_SearchData(collegeName, collegeId);
   }
 
   public abstract String getCollegeName();
 
-  public abstract int getCollegeId();
+  public abstract CollegeId getCollegeId();
 }
