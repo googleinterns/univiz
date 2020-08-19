d1a37376acc1219f4828cb9304f5c6c7fedb45b4
Bianca Macias <bmac@biancamacias.com>
2020-08-19 19:40:20 +0000

    added to test

--- capstone/src/test/java/com/google/univiz/api/resource/MapsResourceImplTest.java
+++ capstone/src/test/java/com/google/univiz/api/resource/MapsResourceImplTest.java
@@ -1,9 +1,16 @@
 package com.google.univiz.api.resource;
 
+import static com.google.common.truth.Truth.assertThat;
+
 import com.google.inject.Guice;
 import com.google.inject.testing.fieldbinder.Bind;
 import com.google.inject.testing.fieldbinder.BoundFieldModule;
 import com.google.univiz.api.CollegeDataApi;
+import com.google.univiz.api.representation.CollegeData;
+import com.google.univiz.api.representation.CollegeId;
+import com.google.univiz.api.representation.MapsData;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import org.junit.Before;
 import org.junit.Rule;
@@ -23,11 +30,25 @@ public final class MapsResourceImplTest {
 
   @Inject private MapsResourceImpl mapsImpl;
 
+  private static final int COLLEGE_ID = 193900;
+
   @Before
   public void guiceSetUp() {
     Guice.createInjector(BoundFieldModule.of(this)).injectMembers(this);
   }
 
   @Test
-  public void testMapsResourceImpl() {}
+  public void testMapsResourceImpl() {
+    CollegeId id = CollegeId.create(COLLEGE_ID);
+    List<CollegeId> ids = new ArrayList<>();
+    ids.add(id);
+    assertThat(ids).hasSize(1);
+
+    List<MapsData> mapsData = mapsImpl.getMapsData(ids);
+    assertThat(mapsData).hasSize(1);
+
+    MapsData college = mapsData.get(0);
+    assertThat(college.name()).isEqualTo("New York University");
+    assertThat(college.latitude()).isEqualTo(40.729452);
+  }
 }
