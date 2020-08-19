917a30ac03e11b6fb3440fccb951e55f8117de19
Kaveh Shahabi <shahabi@google.com>
2020-08-18 17:30:34 -0700

    Add a helper class for servlet classes

---
+++ capstone/src/test/java/com/google/univiz/common/ServletHelperTest.java
@@ -0,0 +1,52 @@
+package com.google.univiz.common;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class ServletHelperTest {
+
+  private static final Gson GSON = new Gson();
+
+  @Test
+  public void writeJsonToResponse_singleString() throws IOException {
+    String response = writeJsonToResponse("some input");
+    assertThat(response).isEqualTo("\"some input\"\n");
+  }
+
+  @Test
+  public void writeJsonToResponse_null() throws IOException {
+    String response = writeJsonToResponse(null);
+    assertThat(response).isEqualTo("null\n");
+  }
+
+  @Test
+  public void writeJsonToResponse_stringArray() throws IOException {
+    String response = writeJsonToResponse(Arrays.asList("item 1", "item 2"));
+    assertThat(response).isEqualTo("[\"item 1\",\"item 2\"]\n");
+  }
+
+  private String writeJsonToResponse(Object input) throws IOException {
+    HttpServletResponse mockResponse = mock(HttpServletResponse.class);
+    StringWriter stringWriter = new StringWriter();
+    PrintWriter writer = new PrintWriter(stringWriter);
+    when(mockResponse.getWriter()).thenReturn(writer);
+
+    ServletHelper.writeJsonToResponse(GSON, mockResponse, input);
+
+    verify(mockResponse).setContentType("application/json");
+    return stringWriter.toString();
+  }
+}
