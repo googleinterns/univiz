58457de165565a748a6d4e19109dddc490fcfd08
mmurray22 <michaelamurray2357@gmail.com>
2020-08-19 17:02:39 +0000

    Finishes urlprovider test

--- capstone/src/test/java/com/google/univiz/UrlProviderImplTest.java
+++ capstone/src/test/java/com/google/univiz/UrlProviderImplTest.java
@@ -13,8 +13,29 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class UrlProviderImplTest {
+  private static final String frontUrl =
+      "https://api.data.gov/ed/collegescorecard/v1/schools.json?";
+  private static final String paramCollegeName = "school.name=";
+  private static final String paramFields = "&fields=";
+  private static final String paramFieldsVal = "id,school.name";
+  private static final String paramApiKey = "&api_key=";
+  private final UnivizConfig univizConfig;
+  
+  @Inject
+  protected UrlProviderImplTest(UnivizConfig univizConfig) {
+    this.univizConfig = univizConfig;
+  }
+    
   private String createExpectedUrl(String collegeName) {
+    collegeName.replaceAll(" ", "%20");
     StringBuilder urlStringBuilder = new StringBuilder();
+    urlStringBuilder.append(paramCollegeName);
+    urlStringBuilder.append(collegeName);
+    urlStringBuilder.append(paramFields);
+    urlStringBuilder.append(paramFieldsVal);
+    urlStringBuilder.append(paramApiKey);
+    urlStringBuilder.append(univizConfig.scorecardApiKey());
+    return urlStringBuilder.toString();
   }
   
   @Test
