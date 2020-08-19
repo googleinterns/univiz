838aad7c0e186a5d2a7e2ce61b9b45abc1cce06a
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-18 20:12:59 -0700

    Remove fields from result to see if url is constructed correctly

--- capstone/src/test/java/com/google/univiz/scorecard/URLProviderImplTest.java
+++ capstone/src/test/java/com/google/univiz/scorecard/URLProviderImplTest.java
@@ -1,12 +1,15 @@
 package com.google.univiz.scorecard;
 
 import static com.google.common.truth.Truth.assertThat;
+import static java.util.stream.Collectors.joining;
 
 import com.google.inject.Guice;
 import com.google.univiz.api.CollegeId;
 import com.google.univiz.config.UnivizConfigModule;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import javax.inject.Inject;
 import org.junit.Before;
 import org.junit.Rule;
@@ -32,21 +35,34 @@ public final class URLProviderImplTest {
     CollegeId fakeCollegeId2 = CollegeId.create(2);
     CollegeId fakeCollegeId3 = CollegeId.create(3);
     List<CollegeId> ids = Arrays.asList(fakeCollegeId1, fakeCollegeId2, fakeCollegeId3);
-    String actual = testImpl.getUrlFromCollegeIds(ids);
+    String result = testImpl.getUrlFromCollegeIds(ids);
+
+    Set<String> expectedFields = new LinkedHashSet<>();
+    expectedFields.add("id");
+    expectedFields.add("school.name");
+    expectedFields.add("school.city");
+    expectedFields.add("school.main_campus");
+    expectedFields.add("location.lat");
+    expectedFields.add("location.lon");
+    expectedFields.add("school.carnegie_size_setting");
+    expectedFields.add("latest.admissions.admission_rate.overall");
+    expectedFields.add("latest.admissions.sat_scores.average.overall");
+    expectedFields.add("latest.student.size");
+    expectedFields.add("latest.cost.attendance.academic_year");
+    expectedFields.add("latest.student.demographics.men");
+    expectedFields.add("latest.student.demographics.women");
+
     StringBuilder expectedBuilder =
         new StringBuilder("https://api.data.gov/ed/collegescorecard/v1/schools.json?id=");
     expectedBuilder.append(
         String.format("%d,%d,%d", fakeCollegeId1.id(), fakeCollegeId2.id(), fakeCollegeId3.id()));
     expectedBuilder.append("&per_page=1&fields=");
-    expectedBuilder.append("id,school.name,school.city,school.main_campus,");
-    expectedBuilder.append(
-        "location.lat,location.lon,school.carnegie_size_setting,latest.admissions.admission_rate.overall,");
-    expectedBuilder.append("latest.admissions.sat_scores.average.overall,latest.student.size,");
-    expectedBuilder.append("latest.cost.attendance.academic_year,latest.student.demographics.men,");
-    expectedBuilder.append("latest.student.demographics.women&api_key=");
-    expectedBuilder.append("scorecard_test_key");
+    expectedBuilder.append(expectedFields.stream().map(field -> "").collect(joining(",")));
+    expectedBuilder.append("&api_key=scorecard_test_key");
     String expected = expectedBuilder.toString();
 
+    String actual =
+        expectedFields.stream().reduce(result, (temp, field) -> temp.replace(field, ""));
     assertThat(actual).isEqualTo(expected);
   }
 }
