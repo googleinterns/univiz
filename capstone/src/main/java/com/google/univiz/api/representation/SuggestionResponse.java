09123f51b4d12d4ce449e51094e67f8c31c7f6a6
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-08-19 16:51:26 -0700

    Revert "Revert "SuggestionData implementation"" (#98)
    * Revert "Revert "SuggestionData implementation" (#97)"
    
    This reverts commit c4de56346e6ebd13abe2c4f7c137c80dac0b1642.
    
    * Moves many files around and now it passes
    
    * Fixes import problem

---
+++ capstone/src/main/java/com/google/univiz/api/representation/SuggestionResponse.java
@@ -0,0 +1,14 @@
+package com.google.univiz.api.representation;
+
+import com.google.auto.value.AutoValue;
+import com.google.gson.annotations.SerializedName;
+import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
+import java.util.List;
+
+/** SuggestionResponse represents the list of SuggestionData specific for the Search feature */
+@GenerateTypeAdapter
+@AutoValue
+public abstract class SuggestionResponse {
+  @SerializedName("results")
+  public abstract List<SuggestionData> suggestions();
+}
