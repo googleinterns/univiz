3790df8f0d03ea2e2efd77b538d9ad838fe3f3fc
Chris Condit <ccondit@google.com>
2020-08-18 17:32:30 -0700

    Introduces QueryStringUtil

---
+++ capstone/src/test/java/com/google/univiz/servlets/QueryStringUtilTest.java
@@ -0,0 +1,33 @@
+package com.google.univiz.servlets;
+
+import static com.google.common.truth.Truth.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class QueryStringUtilTest {
+
+  @Test
+  public void emptyQueryString_yieldsEmptyMultimap() {
+    assertThat(QueryStringUtil.getQueryParams("")).isEmpty();
+  }
+
+  @Test
+  public void singleParamSingleVal_yieldsMultimap() {
+    assertThat(QueryStringUtil.getQueryParams("foo=bar")).containsExactly("foo", "bar");
+  }
+
+  @Test
+  public void multiParamsSingleVal_yieldsMultimap() {
+    assertThat(QueryStringUtil.getQueryParams("foo=bar&fizz=buzz"))
+        .containsExactly("foo", "bar", "fizz", "buzz");
+  }
+
+  @Test
+  public void multiParamsMultiVals_yieldsMultimap() {
+    assertThat(QueryStringUtil.getQueryParams("foo=bar,baz&fizz=buzz"))
+        .containsExactly("foo", "bar", "foo", "baz", "fizz", "buzz");
+  }
+}
