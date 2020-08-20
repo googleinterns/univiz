c7f00e66ccb7e6b84d4509888c82f3fa6612488a
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-20 10:33:20 -0700

    Add Javadoc to interfaces

--- capstone/src/main/java/com/google/univiz/scorecard/URLProvider.java
+++ capstone/src/main/java/com/google/univiz/scorecard/URLProvider.java
@@ -3,7 +3,13 @@ package com.google.univiz.scorecard;
 import com.google.univiz.api.CollegeId;
 import java.util.List;
 
+/** Construct a URL string based on the user's preferred colleges. */
 interface URLProvider {
 
+  /**
+   * Return URL in String form to a remote resource corresponding to the given CollegeIds.
+   *
+   * @param ids the CollegeIds of the user's preferred colleges
+   */
   String getUrlFromCollegeIds(List<CollegeId> ids);
 }
