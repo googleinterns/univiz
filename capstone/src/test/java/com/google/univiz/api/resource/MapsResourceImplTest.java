0fb4f63b89c26786aaa6a4826c845c51b1f0eac6
Bianca Macias <bmac@biancamacias.com>
2020-08-20 16:39:07 +0000

    nits

--- capstone/src/test/java/com/google/univiz/api/resource/MapsResourceImplTest.java
+++ capstone/src/test/java/com/google/univiz/api/resource/MapsResourceImplTest.java
@@ -1,6 +1,7 @@
 package com.google.univiz.api.resource;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Lists;
