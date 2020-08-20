f23d90810e243b91acf790f3c76b8c6920772134
Ihsan Olawale <43079005+ihsan314@users.noreply.github.com>
2020-08-20 14:42:44 -0700

    College data api impl (#82)
    * Initialize CollegeDataApiImpl.java
    
    * Move CollegeDataApiImpl to scorecard and add method for Reader
    
    * Skeleton of test
    
    * Integrate ScorecardResponse into CollegeDataApiImpl
    
    * Add test for CollegeDataApiImpl and add interface for getReaderFromCollegeId
    
    * Make tests compile
    
    * Format files properly
    
    * s/convert/doForward
    
    * Added GenerateTypeAdapter to CollegeDataApiImpl
    
    * Proper formatting of CollegeDataApiImpl.java
    
    * Address nits, adjust scope of classes/members, throw IOException
    
    * Make only one REST call instead of one per CollegeId
    
    * Add getStreamFromUrl method
    
    * Use streams to convert ScorecardData to CollegeData
    
    * Remove extraneous comment
    
    * Update capstone/src/main/java/com/google/univiz/scorecard/CollegeDataApiImpl.java
    
    Co-authored-by: Chris Condit <ccondit@users.noreply.github.com>
    
    * Use collect instead of forEach
    
    * Switch to StringBuilder to use streams
    
    * Switch to StringJoiner for inserting commas between ids
    
    * Put fields in Set and use StringJoiner to append to URL
    
    * Resolve formatting errors
    
    * Remove extraneous instantiation of ScorecardConverter
    
    * Apply suggestions from code review
    
    - Make `CollegeIdReaderProviderImpl` final
    - Return `url.openStream()` inline
    
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>
    
    * Auto-generate fields set from ScorecardData annotations
    
    * Refactor getUrlFromCollegeIds into URLProvider and adjust test
    
    * Properly format files
    
    * Add test for URLProviderImpl
    
    * Format test file properly
    
    * Change getDeclaredFields to getDeclaredMethods
    
    * Inject Gson and format properly
    
    * Apply suggestions from code review
    
    - go more extreme with method refs
    - remove extraneous BoundFieldModule initialization
    
    Co-authored-by: Chris Condit <ccondit@users.noreply.github.com>
    
    * Remove unneeded import, break up long line, label class final
    
    * Use LinkedHashSet to keep Set of fields deterministic
    
    * s/Integer::toString/String::valueOf to remove ambiguity
    
    * Test scorecard api injection and strengthen url check
    
    * Remove fields from result to see if url is constructed correctly
    
    * Account for all instances of fields being removed
    
    * Added empty/multiple results tests
    
    * Add test for IOException being thrown
    
    * Update capstone/src/main/java/com/google/univiz/scorecard/URLProviderImpl.java
    
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>
    
    * Load all colleges on the first page
    
    * Sort fields Set to make it deterministic
    
    * Spy on CollegeIdReaderProviderImpl.getStreamUrl for exception testing
    
    * Add Javadoc to interfaces
    
    * Use collegeId2 as a list element in testMultipleResults
    
    * Strengthen testGetUrlFromCollegeIds
    
    * Apply suggestions from code review
    
    - Remove protected
    - Add @Override annotation
    
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>
    
    * Move files to appropriate locations
    
    Co-authored-by: Chris Condit <ccondit@users.noreply.github.com>
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>

--- capstone/src/test/java/com/google/univiz/api/representation/CollegeDataStatsConverterTest.java
+++ capstone/src/test/java/com/google/univiz/api/representation/CollegeDataStatsConverterTest.java
@@ -3,9 +3,6 @@ package com.google.univiz.api.representation;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 
-import com.google.univiz.api.representation.CarnegieSizeDegree;
-import com.google.univiz.api.representation.CollegeData;
-import com.google.univiz.api.representation.CollegeId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
