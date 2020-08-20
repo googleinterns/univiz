e2fad2cf333ba4f986560d295d587c34d93ba17f
Bianca Macias <bmac@biancamacias.com>
2020-08-20 23:06:56 +0000

    fixed tests to be more concise and readable

--- capstone/src/main/java/com/google/univiz/api/resource/MapsResource.java
+++ capstone/src/main/java/com/google/univiz/api/resource/MapsResource.java
@@ -2,6 +2,7 @@ package com.google.univiz.api.resource;
 
 import com.google.univiz.api.representation.CollegeId;
 import com.google.univiz.api.representation.MapsData;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -13,5 +14,5 @@ public interface MapsResource {
    * Takes a list of college ids and returns a list of MapsData objects to the caller. This object
    * contains all data relevant to the Maps feature.
    */
-  List<MapsData> getMapsData(List<CollegeId> colleges);
+  List<MapsData> getMapsData(List<CollegeId> colleges) throws IOException;
 }
--- capstone/src/test/java/com/google/univiz/api/resource/MapsResourceImplTest.java
+++ capstone/src/test/java/com/google/univiz/api/resource/MapsResourceImplTest.java
@@ -12,6 +12,7 @@ import com.google.univiz.api.representation.CarnegieSizeDegree;
 import com.google.univiz.api.representation.CollegeData;
 import com.google.univiz.api.representation.CollegeId;
 import com.google.univiz.api.representation.MapsData;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -33,19 +34,66 @@ public final class MapsResourceImplTest {
 
   @Inject private MapsResourceImpl mapsImpl;
 
-  private static final int COLLEGE_ID = 193900;
-  private static final String NAME = "New York University";
-  private static final String CITY = "New York";
-  private static final boolean FLAG_MAIN_CAMPUS = true;
-  private static final double LATITUDE = 40.729452;
-  private static final double LONGITUDE = -73.997264;
+  private static final CollegeId NYU_COLLEGE_ID = CollegeId.create(193900);
+  private static final String NYU_NAME = "New York University";
+  private static final String NYU_CITY = "New York";
+  private static final boolean NYU_FLAG_MAIN_CAMPUS = true;
+  private static final double NYU_LATITUDE = 40.729452;
+  private static final double NYU_LONGITUDE = -73.997264;
   private static final int CARNEGIE_SIZE_DEGREE = 17;
-  private static final double ADMISSION_RATE = 0.1999;
-  private static final double AVG_SAT = 1419.0;
-  private static final int NUM_OF_UNDERGRADS = 26339;
-  private static final int AVG_COST = 69830;
-  private static final double RATIO_OF_MEN = 0.4253;
-  private static final double RATIO_OF_WOMEN = 0.5747;
+  private static final double NYU_ADMISSION_RATE = 0.1999;
+  private static final double NYU_AVG_SAT = 1419.0;
+  private static final int NYU_NUM_OF_UNDERGRADS = 26339;
+  private static final int NYU_AVG_COST = 69830;
+  private static final double NYU_RATIO_OF_MEN = 0.4253;
+  private static final double NYU_RATIO_OF_WOMEN = 0.5747;
+
+  private static final CollegeId STANFORD_COLLEGE_ID = CollegeId.create(243744);
+  private static final String STANDFORD_NAME = "Stanford University";
+  private static final String STANDFORD_CITY = "Stanford";
+  private static final boolean STANFORD_FLAG_MAIN_CAMPUS = true;
+  private static final double STANFORD_LATITUDE = 37.429434;
+  private static final double STANFORD_LONGITUDE = -122.167359;
+  private static final double STANFORD_ADMISSION_RATE = 0.0436;
+  private static final double STANFORD_AVG_SAT = 1497.0;
+  private static final int STANFORD_NUM_OF_UNDERGRADS = 7083;
+  private static final int STANFORD_AVG_COST = 69109;
+  private static final double STANFORD_RATIO_OF_MEN = 0.4991;
+  private static final double STANFORD_RATIO_OF_WOMEN = 0.5009;
+
+  private static final CollegeData NYU_COLLEGE_DATA =
+      CollegeData.builder()
+          .setId(NYU_COLLEGE_ID)
+          .setName(NYU_NAME)
+          .setCity(NYU_CITY)
+          .setIsMainCampus(NYU_FLAG_MAIN_CAMPUS)
+          .setLatitude(NYU_LATITUDE)
+          .setLongitude(NYU_LONGITUDE)
+          .setCarnegieSizeDegree(CarnegieSizeDegree.getDegree(CARNEGIE_SIZE_DEGREE))
+          .setAdmissionRate(NYU_ADMISSION_RATE)
+          .setAvgSat(NYU_AVG_SAT)
+          .setNumOfUndergrads(NYU_NUM_OF_UNDERGRADS)
+          .setAvgCost(NYU_AVG_COST)
+          .setRatioOfMen(NYU_RATIO_OF_MEN)
+          .setRatioOfWomen(NYU_RATIO_OF_WOMEN)
+          .build();
+
+  private static final CollegeData STANFORD_COLLEGE_DATA =
+      CollegeData.builder()
+          .setId(STANFORD_COLLEGE_ID)
+          .setName(STANDFORD_NAME)
+          .setCity(STANDFORD_CITY)
+          .setIsMainCampus(STANFORD_FLAG_MAIN_CAMPUS)
+          .setLatitude(STANFORD_LATITUDE)
+          .setLongitude(STANFORD_LONGITUDE)
+          .setCarnegieSizeDegree(CarnegieSizeDegree.getDegree(CARNEGIE_SIZE_DEGREE))
+          .setAdmissionRate(STANFORD_ADMISSION_RATE)
+          .setAvgSat(STANFORD_AVG_SAT)
+          .setNumOfUndergrads(STANFORD_NUM_OF_UNDERGRADS)
+          .setAvgCost(STANFORD_AVG_COST)
+          .setRatioOfMen(STANFORD_RATIO_OF_MEN)
+          .setRatioOfWomen(STANFORD_RATIO_OF_WOMEN)
+          .build();
 
   @Before
   public void guiceSetUp() {
@@ -53,32 +101,16 @@ public final class MapsResourceImplTest {
   }
 
   @Test
-  public void testMapsResourceImpl() {
-    CollegeId id = CollegeId.create(COLLEGE_ID);
-    List<CollegeId> ids = Lists.newArrayList(id);
+  public void testMapsResourceImpl() throws IOException {
+    List<CollegeId> ids = Lists.newArrayList(NYU_COLLEGE_ID);
     assertThat(ids).hasSize(1);
 
-    CollegeData college =
-        CollegeData.builder()
-            .setId(id)
-            .setName(NAME)
-            .setCity(CITY)
-            .setIsMainCampus(FLAG_MAIN_CAMPUS)
-            .setLatitude(LATITUDE)
-            .setLongitude(LONGITUDE)
-            .setCarnegieSizeDegree(CarnegieSizeDegree.getDegree(CARNEGIE_SIZE_DEGREE))
-            .setAdmissionRate(ADMISSION_RATE)
-            .setAvgSat(AVG_SAT)
-            .setNumOfUndergrads(NUM_OF_UNDERGRADS)
-            .setAvgCost(AVG_COST)
-            .setRatioOfMen(RATIO_OF_MEN)
-            .setRatioOfWomen(RATIO_OF_WOMEN)
-            .build();
-    List<CollegeData> collegesData = Lists.newArrayList(college);
+    List<CollegeData> collegesData = Lists.newArrayList(NYU_COLLEGE_DATA);
     assertThat(collegesData).hasSize(1);
 
     when(collegeDataApi.getCollegesById(ids)).thenReturn(collegesData);
     List<MapsData> mapsData = mapsImpl.getMapsData(ids);
+    assertThat(mapsData).hasSize(1);
 
     MapsData collegeMapData = mapsData.get(0);
     assertThat(collegeMapData.name()).isEqualTo("New York University");
@@ -86,60 +118,27 @@ public final class MapsResourceImplTest {
   }
 
   @Test
-  public void testEmptyMapsResourcesImpl() {
+  public void testEmptyMapsResourcesImpl() throws IOException {
     List<CollegeId> ids = new ArrayList<>();
     List<CollegeData> collegesData = new ArrayList<>();
 
     when(collegeDataApi.getCollegesById(ids)).thenReturn(collegesData);
+    assertThat(collegesData).isEmpty();
     List<MapsData> mapsData = mapsImpl.getMapsData(ids);
     assertThat(mapsData).isEmpty();
   }
 
   @Test
-  public void testManyMapsResourcesImpl() {
-    CollegeId id1 = CollegeId.create(COLLEGE_ID);
-    CollegeId id2 = CollegeId.create(243744);
-    List<CollegeId> ids = Lists.newArrayList(id1, id2);
+  public void testManyMapsResourcesImpl() throws IOException {
+    List<CollegeId> ids = Lists.newArrayList(NYU_COLLEGE_ID, STANFORD_COLLEGE_ID);
     assertThat(ids).hasSize(2);
 
-    CollegeData college1 =
-        CollegeData.builder()
-            .setId(id1)
-            .setName(NAME)
-            .setCity(CITY)
-            .setIsMainCampus(FLAG_MAIN_CAMPUS)
-            .setLatitude(LATITUDE)
-            .setLongitude(LONGITUDE)
-            .setCarnegieSizeDegree(CarnegieSizeDegree.getDegree(CARNEGIE_SIZE_DEGREE))
-            .setAdmissionRate(ADMISSION_RATE)
-            .setAvgSat(AVG_SAT)
-            .setNumOfUndergrads(NUM_OF_UNDERGRADS)
-            .setAvgCost(AVG_COST)
-            .setRatioOfMen(RATIO_OF_MEN)
-            .setRatioOfWomen(RATIO_OF_WOMEN)
-            .build();
-    CollegeData college2 =
-        CollegeData.builder()
-            .setId(id2)
-            .setName("Stanford University")
-            .setCity("Stanford")
-            .setIsMainCampus(true)
-            .setLatitude(37.429434)
-            .setLongitude(-122.167359)
-            .setCarnegieSizeDegree(CarnegieSizeDegree.getDegree(CARNEGIE_SIZE_DEGREE))
-            .setAdmissionRate(0.0436)
-            .setAvgSat(1497.0)
-            .setNumOfUndergrads(7083)
-            .setAvgCost(69109)
-            .setRatioOfMen(0.4991)
-            .setRatioOfWomen(0.5009)
-            .build();
-
-    List<CollegeData> collegesData = Lists.newArrayList(college1, college2);
+    List<CollegeData> collegesData = Lists.newArrayList(NYU_COLLEGE_DATA, STANFORD_COLLEGE_DATA);
     assertThat(collegesData).hasSize(2);
 
     when(collegeDataApi.getCollegesById(ids)).thenReturn(collegesData);
     List<MapsData> mapsData = mapsImpl.getMapsData(ids);
+    assertThat(mapsData).hasSize(2);
 
     MapsData college1MapData = mapsData.get(0);
     MapsData college2MapData = mapsData.get(1);
