a3f311d69fa6d70b8a1f0fd3088d77ae13bd5e62
Bianca Macias <51244558+biancamacias@users.noreply.github.com>
2020-08-21 11:17:44 -0700

    MapsResource.java class implementation (#87)
    MapsResourceImpl.java class with tests

---
+++ capstone/src/main/java/com/google/univiz/api/resource/MapsResourceImpl.java
@@ -0,0 +1,28 @@
+package com.google.univiz.api.resource;
+
+import com.google.univiz.api.CollegeDataApi;
+import com.google.univiz.api.representation.CollegeData;
+import com.google.univiz.api.representation.CollegeId;
+import com.google.univiz.api.representation.MapsData;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.inject.Inject;
+
+final class MapsResourceImpl implements MapsResource {
+  private final CollegeDataApi collegeDataApi;
+  private final CollegeDataConverter converter;
+
+  @Inject
+  MapsResourceImpl(CollegeDataApi collegeDataApi, CollegeDataConverter converter) {
+    this.collegeDataApi = collegeDataApi;
+    this.converter = converter;
+  }
+
+  @Override
+  public List<MapsData> getMapsData(List<CollegeId> ids) throws IOException {
+    List<CollegeData> colleges = collegeDataApi.getCollegesById(ids);
+
+    return colleges.stream().map(converter::convert).collect(Collectors.toList());
+  }
+}
