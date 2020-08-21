1cf007eac2a10ced43398387f540d6dad168123e
Kaveh Shahabi <shahabi@google.com>
2020-08-20 17:40:24 -0700

    Adds an startup-like test that tests whether all existing servlets can be constructed using guice. I'm adding an empty servlet as a proof of concept.

--- capstone/src/main/java/com/google/univiz/UnivizBootstrapModule.java
+++ capstone/src/main/java/com/google/univiz/UnivizBootstrapModule.java
@@ -1,11 +1,17 @@
 package com.google.univiz;
 
 import com.google.inject.AbstractModule;
+import com.google.univiz.api.CollegeApiModule;
+import com.google.univiz.api.resource.ResourceModule;
+import com.google.univiz.config.UnivizConfigModule;
 
 final class UnivizBootstrapModule extends AbstractModule {
 
   @Override
   protected void configure() {
+    install(new UnivizConfigModule());
     install(new GsonModule());
+    install(new ResourceModule());
+    install(new CollegeApiModule());
   }
 }
