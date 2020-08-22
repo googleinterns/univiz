683c7f1edb7d367bee14ba1d8a6f6df8b1d8f61f
Kaveh Shahabi <shahabi@google.com>
2020-08-21 17:08:56 -0700

    Adds empty implementation classes for all the existing interfaces. This would enable me to write startup tests for boostrap module. (#101)

---
+++ capstone/src/main/java/com/google/univiz/api/SuggestionDataApiImpl.java
@@ -0,0 +1,11 @@
+package com.google.univiz.api;
+
+import com.google.univiz.api.representation.SuggestionResponse;
+
+final class SuggestionDataApiImpl implements SuggestionDataApi {
+
+  @Override
+  public SuggestionResponse getCollegeSuggestions(String collegeName) {
+    throw new UnsupportedOperationException();
+  }
+}
