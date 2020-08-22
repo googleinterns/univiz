d2cd21e412026542e8da3253b7e5f55c78f0e5d6
Chris Condit <ccondit@google.com>
2020-08-21 17:09:07 -0700

    Fix Java formatting.

--- capstone/src/main/java/com/google/univiz/GuiceServletConfig.java
+++ capstone/src/main/java/com/google/univiz/GuiceServletConfig.java
@@ -6,23 +6,23 @@ import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
 import com.google.univiz.servlets.CollegeDataVisualizationService;
 
-/**
- * Context listener to bootstap Guice based servlets.
- */
+/** Context listener to bootstap Guice based servlets. */
 public class GuiceServletConfig extends GuiceServletContextListener {
 
   @Override
   protected Injector getInjector() {
-    return Guice.createInjector(new UnivizBootstrapModule(), new ServletModule() {
-      @Override
-      protected void configureServlets() {
-        serve("/viz/" + CollegeDataVisualizationService.DEADLINES_SUFFIX)
-            .with(CollegeDataVisualizationService.class);
-        serve("/viz/" + CollegeDataVisualizationService.STATS_SUFFIX)
-            .with(CollegeDataVisualizationService.class);
-        serve("/viz/" + CollegeDataVisualizationService.TIMELINES_SUFFIX)
-            .with(CollegeDataVisualizationService.class);
-      }
-    });
+    return Guice.createInjector(
+        new UnivizBootstrapModule(),
+        new ServletModule() {
+          @Override
+          protected void configureServlets() {
+            serve("/viz/" + CollegeDataVisualizationService.DEADLINES_SUFFIX)
+                .with(CollegeDataVisualizationService.class);
+            serve("/viz/" + CollegeDataVisualizationService.STATS_SUFFIX)
+                .with(CollegeDataVisualizationService.class);
+            serve("/viz/" + CollegeDataVisualizationService.TIMELINES_SUFFIX)
+                .with(CollegeDataVisualizationService.class);
+          }
+        });
   }
 }
