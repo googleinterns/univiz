f0b5cdd9731e20abb7aac7be40311546330785b4
Kaveh Shahabi <shahabi@google.com>
2020-08-21 17:18:16 -0700

    Fix ResourceModule compilation error

--- capstone/src/main/java/com/google/univiz/api/resource/ResourceModule.java
+++ capstone/src/main/java/com/google/univiz/api/resource/ResourceModule.java
@@ -1,7 +1,6 @@
 package com.google.univiz.api.resource;
 
 import com.google.inject.AbstractModule;
-import com.google.univiz.api.MapsResource;
 
 /**
  * Adds bindings for all the resource interfaces. These interfaces sit between the backend layer
