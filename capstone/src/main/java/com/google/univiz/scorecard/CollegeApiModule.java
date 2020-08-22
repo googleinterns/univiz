dcd148dec3c4560e0abcd13ff3401fdd09a6fce5
Kaveh Shahabi <shahabi@google.com>
2020-08-21 17:14:46 -0700

    Move CollegeApiModule into scorecard for better visibility to impl classes

--- capstone/src/main/java/com/google/univiz/scorecard/CollegeApiModule.java
+++ capstone/src/main/java/com/google/univiz/scorecard/CollegeApiModule.java
@@ -1,6 +1,8 @@
-package com.google.univiz.api;
+package com.google.univiz.scorecard;
 
 import com.google.inject.AbstractModule;
+import com.google.univiz.api.CollegeDataApi;
+import com.google.univiz.api.SuggestionDataApi;
 
 /**
  * Adds bindings for all the interfaces for college data api implementations. They are currently
