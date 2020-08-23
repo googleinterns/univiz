d34bfea3e939d954f1c30302bab8985860dcd5d3
mmurray22 <36782608+mmurray22@users.noreply.github.com>
2020-08-22 20:38:00 -0700

    Update capstone/src/main/java/com/google/univiz/api/SuggestionDataApiImpl.java
    Co-authored-by: Kaveh Shahabi <shahabi@google.com>

--- capstone/src/main/java/com/google/univiz/api/SuggestionDataApiImpl.java
+++ capstone/src/main/java/com/google/univiz/api/SuggestionDataApiImpl.java
@@ -21,7 +21,7 @@ final class SuggestionDataApiImpl implements SuggestionDataApi {
     this.gson = gson;
   }
 
-  // Takes REST API Json response and converts it to SuggestionData
+  /** Takes REST API Json response and converts it to SuggestionData */
   private SuggestionResponse convertJsonToSuggestionResponse(InputStreamReader suggestionReader) {
     return gson.fromJson(suggestionReader, SuggestionResponse.class);
   }
