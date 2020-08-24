05432021a2d0f377173fef049d72c24aad605cff
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-24 10:33:36 -0700

    Add fields to Deadline class

--- capstone/src/main/java/com/google/univiz/api/representation/Deadline.java
+++ capstone/src/main/java/com/google/univiz/api/representation/Deadline.java
@@ -7,4 +7,39 @@ import com.google.auto.value.AutoValue;
  * for the purpose of graphing due dates.
  */
 @AutoValue
-public abstract class Deadline {}
+public abstract class Deadline {
+  public abstract int openingMonth();
+
+  public abstract int openingDay();
+
+  public abstract int openingYear();
+
+  public abstract int closingMonth();
+
+  public abstract int closingDay();
+
+  public abstract int closingYear();
+
+  public abstract String name();
+
+  public static Builder builder() {
+    return new AutoValue_Deadline.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setOpeningMonth(int value);
+
+    public abstract Builder setOpeningDay(int value);
+
+    public abstract Builder setOpeningYear(int value);
+
+    public abstract Builder setClosingMonth(int value);
+
+    public abstract Builder setClosingDay(int value);
+
+    public abstract Builder setClosingYear(int value);
+
+    public abstract Deadline build();
+  }
+}
