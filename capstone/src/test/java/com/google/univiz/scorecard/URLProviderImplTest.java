b10c3a7f376392d7520e9360836296415c1220aa
mmurray22 <michaelamurray2357@gmail.com>
2020-08-24 17:15:51 +0000

    Updates in response to PR comments

--- capstone/src/test/java/com/google/univiz/scorecard/URLProviderImplTest.java
+++ capstone/src/test/java/com/google/univiz/scorecard/URLProviderImplTest.java
@@ -25,7 +25,22 @@ public final class URLProviderImplTest {
   private static final String QUERY_SCHOOL_NAME = "school.name=";
   private static final String QUERY_TYPE_ID = "id=";
   private static final String PER_PAGE = "&per_page=";
-  private static final String SUGGESTION_FIELDS = "&fields=id,school.name";
+  private static final String FIELDS_PARAM = "&fields=";
+  private static final String SUGGESTION_FIELDS = "id,school.name";
+  private static final String ID_FIELD = "id";
+  private static final String SCHOOL_NAME = "school.name";
+  private static final String SCHOOL_CITY = "school.city";
+  private static final String SCHOOL_MAIN_CAMPUS = "school.main_campus";
+  private static final String LOCATION_LAT = "location.lat";
+  private static final String LOCATION_LON = "location.lon";
+  private static final String CARNEGIE_SIZE_SETTING = "school.carnegie_size_setting";
+  private static final String ADMISSION_RATE = "latest.admissions.admission_rate.overall";
+  private static final String SAT_SCORES = "latest.admissions.sat_scores.average.overall";
+  private static final String SIZE = "latest.student.size";
+  private static final String ACADEMIC_YEAR = "latest.cost.attendance.academic_year";
+  private static final String MEN = "latest.student.demographics.men";
+  private static final String WOMEN = "latest.student.demographics.women";
+  private static final String API_KEY = "&api_key=scorecard_test_key";
   @Rule public final MockitoRule rule = MockitoJUnit.rule();
   @Inject private URLProviderImpl testImpl;
 
@@ -43,8 +58,9 @@ public final class URLProviderImplTest {
     expectedBuilder.append(FRONT_URL);
     expectedBuilder.append(QUERY_SCHOOL_NAME);
     expectedBuilder.append(partialCollegeName);
+    expectedBuilder.append(FIELDS_PARAM);
     expectedBuilder.append(SUGGESTION_FIELDS);
-    expectedBuilder.append("&api_key=scorecard_test_key");
+    expectedBuilder.append(API_KEY);
     String expected = expectedBuilder.toString();
 
     assertThat(actual).isEqualTo(expected);
@@ -59,27 +75,30 @@ public final class URLProviderImplTest {
     String actual = testImpl.getDataUrl(ids);
 
     Set<String> expectedFields = new LinkedHashSet<>();
-    expectedFields.add("id");
-    expectedFields.add("school.name");
-    expectedFields.add("school.city");
-    expectedFields.add("school.main_campus");
-    expectedFields.add("location.lat");
-    expectedFields.add("location.lon");
-    expectedFields.add("school.carnegie_size_setting");
-    expectedFields.add("latest.admissions.admission_rate.overall");
-    expectedFields.add("latest.admissions.sat_scores.average.overall");
-    expectedFields.add("latest.student.size");
-    expectedFields.add("latest.cost.attendance.academic_year");
-    expectedFields.add("latest.student.demographics.men");
-    expectedFields.add("latest.student.demographics.women");
+    expectedFields.add(ID_FIELD);
+    expectedFields.add(SCHOOL_NAME);
+    expectedFields.add(SCHOOL_CITY);
+    expectedFields.add(SCHOOL_MAIN_CAMPUS);
+    expectedFields.add(LOCATION_LAT);
+    expectedFields.add(LOCATION_LON);
+    expectedFields.add(CARNEGIE_SIZE_SETTING);
+    expectedFields.add(ADMISSION_RATE);
+    expectedFields.add(SAT_SCORES);
+    expectedFields.add(SIZE);
+    expectedFields.add(ACADEMIC_YEAR);
+    expectedFields.add(MEN);
+    expectedFields.add(WOMEN);
 
     StringBuilder expectedBuilder =
-        new StringBuilder("https://api.data.gov/ed/collegescorecard/v1/schools.json?id=");
+        new StringBuilder(FRONT_URL);
+    expectedBuilder.append(QUERY_TYPE_ID);
     expectedBuilder.append(
         String.format("%d,%d,%d", fakeCollegeId1.id(), fakeCollegeId2.id(), fakeCollegeId3.id()));
-    expectedBuilder.append("&per_page=3&fields=");
+    expectedBuilder.append(PER_PAGE);
+    expectedBuilder.append("3");
+    expectedBuilder.append(FIELDS_PARAM);
     expectedBuilder.append(expectedFields.stream().sorted().collect(joining(",")));
-    expectedBuilder.append("&api_key=scorecard_test_key");
+    expectedBuilder.append(API_KEY);
     String expected = expectedBuilder.toString();
 
     assertThat(actual).isEqualTo(expected);
