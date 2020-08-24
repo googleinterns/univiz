efc854766833a762cf61af5119c2f0b04e047026
Bianca Macias <bmac@biancamacias.com>
2020-08-24 21:40:12 +0000

    formatting nits

--- capstone/src/test/javascript/mapsSpec.js
+++ capstone/src/test/javascript/mapsSpec.js
@@ -17,5 +17,6 @@ describe('Map Marker and InfoWindow Display', () => {
         spyOn(window, 'fetch')
             .and.returnValue(Promise.resolve({json: () => mapsJsonData}));
         await fetchData();
+        //TODO(biancamacias): mock? assert functions work? but how?
       });
 });
