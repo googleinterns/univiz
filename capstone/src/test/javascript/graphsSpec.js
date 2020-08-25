d7ec5b810c5ad1713781cbdcb82b0c6a30608401
Ihsan Olawale <43079005+ihsan314@users.noreply.github.com>
2020-08-25 16:26:22 -0700

    Simple bar graph (#35)
    * Prepare graphs.js for data visualization
    
    * Add empty bar graph to index.html
    
    * Spell name of script correctly and add div element for chart
    
    * Label horizontal and vertical axes of graph
    
    * Framework for setting DataTable values from JSON object with CollegeScoreCard data
    
    * Attempt to fix formatting errors
    
    * Add parentheses around arrow function argument and get rid of tabs
    
    * Moved JavaScript file to src/main/webapp/js
    
    * Initialize empty file to test graphs.js file
    
    * Refactor deserialization from graphGrossNetTuition
    
    * Use dashboard.html instead of index.html for charts
    
    * Switch to dynamic loading and add framework for test
    
    * Add semicolon as per the request of npx eslint --fix
    
    * Change name of callback to initTuitionData
    
    * Remove extraneous loading code
    
    * Move TODO to initialization of JSON object
    
    * Address formatting issue
    
    * Moves declaration of objects out of callback
    
    * Follow naming convention for tests
    
    * Switch to arrow function
    
    * Move helper functions out of anonymous function
    
    * Format graphs.js
    
    * Add missing comma
    
    * Use toEqual for deep equality
    
    * Format univiz.conf.js to keep presubmit happy
    
    * Adjust Javadoc formatting
    
    * Mock google.visualization.DataTable
    
    * Add javadoc to mock DataTable
    
    * Remove initTuitionData callback function
    
    * Revert to static loading
    
    * Experiment with dynamic loading and Function objects
    
    * Make deserializeTuitionData asynchronous and add charts api to conf
    
    * Successfully load Charts API and have functions in scope
    
    * Add name variable to CollegeStats
    
    * Implement populateDataTable and rename callback
    
    * Resolve java presubmit errors
    
    * Test deserializeTuitionData instead of populateDataTable
    
    * Make function in it asynchronous
    
    * Use appropriate convention for servlet name
    
    * Add call to setName
    
    * Mock addRow to accept array corresponding to a row
    
    * Use appropriate datatype in JSDoc
    
    * Ignore no-unused-vars rule for graphNetTuition

---
+++ capstone/src/test/javascript/graphsSpec.js
@@ -0,0 +1,56 @@
+const google = {};
+google.visualization = {};
+google.visualization.DataTable = class {
+  /**
+   * Dummy constructor for mock DataTable class.
+   */
+  constructor() {}
+
+  /**
+   * Dummy function for adding rows to mock DataTable class.
+   *
+   * @param {Array} values the array of pairs corresponding to a row.
+   */
+  addRow(values) {}
+
+  /**
+   * Dummy function for adding columns to mock DataTable class.
+   *
+   * @param {String} type the datatype of the column.
+   * @param {String} name the name of the column.
+   */
+  addColumn(type, name) {}
+};
+
+
+describe('Data Table Construction', () => {
+  it('Will add correct columns/rows to DataTable from JSON', async () => {
+    const mockTuitionInfo = [
+      {'name': 'University of British Columbia', 'avgCost': 5000},
+      {'name': 'Simon Fraser University', 'avgCost': 4000},
+    ];
+    spyOn(window, 'fetch')
+        .and.returnValue(Promise.resolve({json: () => mockTuitionInfo}));
+    spyOn(google.visualization.DataTable.prototype, 'addRow');
+    spyOn(google.visualization.DataTable.prototype, 'addColumn');
+    await deserializeTuitionData();
+    expect(google.visualization.DataTable.prototype.addRow)
+        .toHaveBeenCalledWith([
+          mockTuitionInfo[0]['name'],
+          mockTuitionInfo[0]['avgCost'],
+        ]);
+    expect(google.visualization.DataTable.prototype.addRow)
+        .toHaveBeenCalledWith([
+          mockTuitionInfo[1]['name'],
+          mockTuitionInfo[1]['avgCost'],
+        ]);
+    expect(google.visualization.DataTable.prototype.addRow.calls.count())
+        .toEqual(2);
+    expect(google.visualization.DataTable.prototype.addColumn)
+        .toHaveBeenCalledWith('string', 'Your Colleges');
+    expect(google.visualization.DataTable.prototype.addColumn)
+        .toHaveBeenCalledWith('number', 'Tuition Cost (USD)');
+    expect(google.visualization.DataTable.prototype.addColumn.calls.count())
+        .toEqual(2);
+  });
+});
