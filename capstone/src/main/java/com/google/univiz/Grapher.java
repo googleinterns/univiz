24be862da205aa15b81d993899111f3baca2a14d
Ihsan Olawale <ihsanolawale@outlook.com>
2020-08-26 11:32:33 -0700

    Add grapher for Guice wiring

---
+++ capstone/src/main/java/com/google/univiz/Grapher.java
@@ -0,0 +1,22 @@
+package com.google.univiz;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.grapher.graphviz.GraphvizGrapher;
+import com.google.inject.grapher.graphviz.GraphvizModule;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Grapher {
+  public static void main(String... args) throws IOException {
+    String filename = "guice_graph.dot";
+    PrintWriter out = new PrintWriter(new File(filename), "UTF-8");
+    Injector demoInjector = Guice.createInjector(new UnivizBootstrapModule());
+    Injector injector = Guice.createInjector(new GraphvizModule());
+    GraphvizGrapher grapher = injector.getInstance(GraphvizGrapher.class);
+    grapher.setOut(out);
+    grapher.setRankdir("TB");
+    grapher.graph(demoInjector);
+  }
+}
