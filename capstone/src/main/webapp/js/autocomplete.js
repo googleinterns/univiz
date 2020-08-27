76bb5a14f83454626eca1bbec9af3694c5d3d682
mmurray22 <michaelamurray2357@gmail.com>
2020-08-27 08:14:27 +0000

    Formats js

--- capstone/src/main/webapp/js/autocomplete.js
+++ capstone/src/main/webapp/js/autocomplete.js
@@ -1,4 +1,4 @@
-/* eslint no-unused-vars: "error"*/ 
+/* eslint-disable no-unused-vars */ 
 
 /*
  * Largely inspired by JS code from W3Schools: https://www.w3schools.com/howto/howto_js_autocomplete.asp
