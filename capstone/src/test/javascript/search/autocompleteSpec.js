8a3cdf76118d5d0183950a854f01e538bde0961a
mmurray22 <michaelamurray2357@gmail.com>
2020-08-31 18:50:22 +0000

    Adds autocomplete tests

---
+++ capstone/src/test/javascript/search/autocompleteSpec.js
@@ -0,0 +1,17 @@
+describe('Autocomplete Suggests correct array', () => {
+  it('Tests Proper List Element Creation Simple', () => {
+    const arrElt = {school.name: 'Hello', collegeId: {id: 0}};
+    const val = 'He';
+    const listElmtActual = createListElmt(arrElt, val);
+    const listElmtExpected = '<div><strong>He</strong>llo</div>';
+    expect(listElmtExpected).toEqual(listElmtActual);
+  });
+  
+  it('Tests Proper List Element Creation Complex', () => {
+    const arrElt = {school.name: 'Hallo Hello', collegeId: {id: 0}};
+    const val = 'He';
+    const listElmtActual = createListElmt(arrElt, val);
+    const listElmtExpected = '<div>Hallo <strong>He</strong>llo</div>';
+    expect(listElmtExpected).toEqual(listElmtActual);
+  });
+});
