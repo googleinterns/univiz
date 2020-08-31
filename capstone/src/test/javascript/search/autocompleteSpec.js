c0624671f5f95ee00adbb715035b87ef6722ac48
mmurray22 <michaelamurray2357@gmail.com>
2020-08-31 19:21:07 +0000

    Updates spec

--- capstone/src/test/javascript/search/autocompleteSpec.js
+++ capstone/src/test/javascript/search/autocompleteSpec.js
@@ -2,7 +2,7 @@ describe('Autocomplete Suggests correct array', () => {
   it('Tests Proper List Element Creation Simple', () => {
     const arrElt = 'Hello';
     const val = 'He';
-    const listElmtActual = createListElmt(arrElt, val);
+    const listElmtActual = createListElmt(arrElt, val).toString();
     const listElmtExpected = '<div><strong>He</strong>llo</div>';
     expect(listElmtExpected).toEqual(listElmtActual);
   });
@@ -10,7 +10,7 @@ describe('Autocomplete Suggests correct array', () => {
   it('Tests Proper List Element Creation Complex', () => {
     const arrElt = 'Hallo Hello';
     const val = 'He';
-    const listElmtActual = createListElmt(arrElt, val);
+    const listElmtActual = createListElmt(arrElt, val).toString();
     const listElmtExpected = '<div>Hallo <strong>He</strong>llo</div>';
     expect(listElmtExpected).toEqual(listElmtActual);
   });
