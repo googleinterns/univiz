b8f0252c021fb8b26baa22464c17d59b4019e85e
mmurray22 <michaelamurray2357@gmail.com>
2020-08-31 19:02:31 +0000

    Updates autocomplete test

--- capstone/src/test/javascript/search/autocompleteSpec.js
+++ capstone/src/test/javascript/search/autocompleteSpec.js
@@ -8,7 +8,7 @@ describe('Autocomplete Suggests correct array', () => {
   });
   
   it('Tests Proper List Element Creation Complex', () => {
-    const arrElt = {school.name: 'Hallo Hello', collegeId: {id: 0}};
+    const arrElt = {'school.name': 'Hallo Hello', 'collegeId': {'id': 0}};
     const val = 'He';
     const listElmtActual = createListElmt(arrElt, val);
     const listElmtExpected = '<div>Hallo <strong>He</strong>llo</div>';
