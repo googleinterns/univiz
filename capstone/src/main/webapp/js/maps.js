ad7956fc4e401b1f6c812a991aed50c7f80e76d5
Bianca Macias <51244558+biancamacias@users.noreply.github.com>
2020-09-01 18:06:48 -0400

    Displaying Map Feature (#112)
    added map feature

---
+++ capstone/src/main/webapp/js/maps.js
@@ -0,0 +1,70 @@
+/* eslint no-unused-vars: ["error", {"varsIgnorePattern": "fetchData"}] */
+
+/**
+ * Function to fetch data from servlet as JSON and calls createMap().
+ */
+async function fetchData() {
+  const queryUrl = new URL(window.location.href);
+  const ids = queryUrl.searchParams.get('id');
+  await fetch('/maps?id=' + ids)
+      .then((response) => response.json())
+      .then((mapsJsonData) => {
+        console.log('Fetching data...');
+        console.log(mapsJsonData);
+        createMap(mapsJsonData);
+      });
+}
+
+/**
+ * Function that creates map markers and info windows.
+ * The map object that is created is centered on the geographic
+ * center of the United States in Lebanon, Kansas. Since this
+ * function does not utilize a bound setting method, this is to ensure
+ * the map is rendered on the United States.
+ * @param {JSON} mapsDataFromFetch JSON response from servlet
+ */
+function createMap(mapsDataFromFetch) {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 39.8097343, lng: -98.5556199}, zoom: 5});
+
+  const infoWindow = new google.maps.InfoWindow();
+  for (const mapsData of mapsDataFromFetch) {
+    const latitudeLongitude = new google.maps.LatLng(
+        mapsData['latitude'],
+        mapsData['longitude']);
+
+    const marker = new google.maps.Marker({
+      position: latitudeLongitude,
+      map: map,
+      title: mapsData['name'],
+    });
+
+    ((marker, mapsData) => {
+      google.maps.event.addListener(marker, 'click',
+          () => {
+            infoWindow.setContent(setDescription(
+                mapsData['name'],
+                mapsData['city'],
+                mapsData['isMainCampus']));
+            infoWindow.open(map, marker);
+          });
+    })(marker, mapsData);
+  }
+}
+
+/**
+ * Helper function for setting content description for info windows.
+ * @param {string} name Name of college/university/institution
+ * @param {string} city City where college/university/institution is located in
+ * @param {boolean} isMainCampus True if it is the main campus, false otherwise
+ * @return {string} Description that will be used for info window content
+ */
+function setDescription(name, city, isMainCampus) {
+  const description = name + ' is located in ' + city + '. ';
+  if (isMainCampus) {
+    return description + 'This is the main campus.';
+  } else {
+    return description + 'This is not the main campus.';
+  }
+}
---
+++ capstone/src/test/javascript/mapsSpec.js
@@ -0,0 +1,136 @@
+google.maps = {};
+google.maps.Map = class {
+  /**
+    * @param {HTMLDivElement} div Div from HTML that map will be displayed in
+    * @param {Object} options Options for centering and viewing maps in HTML
+    */
+  emptyMethod(div, options) {};
+  /**
+    * @param {HTMLDivElement} div Div from HTML that map will be displayed in
+    * @param {Object} options Options for centering and viewing maps in HTML
+    */
+  constructor(div, options) {};
+};
+
+google.maps.LatLng = class {
+  /**
+    * @param {float} lat Location measured in latitude
+    * @param {float} lng Location measured in longitude
+    */
+  emptyMethod(lat, lng) {};
+  /**
+    * @param {float} lat Location measured in latitude
+    * @param {float} lng Location measured in longitude
+    */
+  constructor(lat, lng) {
+    this.emptyMethod(lat, lng);
+  };
+};
+
+google.maps.Marker = class {
+  /**
+    * @param {google.maps.LatLon} latLon google.maps.LatLon object
+    * @param {google.maps.Map} map google.maps.Map object
+    * @param {string} title Name of college/university/institution
+    */
+  emptyMethod(latLon, map, title) {};
+  /**
+    * @param {google.maps.LatLon} latLon google.maps.LatLon object
+    * @param {google.maps.Map} map google.maps.Map object
+    * @param {string} title Name of college/university/institution
+    */
+  constructor(latLon, map, title) {};
+};
+
+google.maps.InfoWindow = class {
+  /** Creates an info window */
+  emptyMethod() {};
+  /** Creates an info window */
+  constructor() {};
+
+  /**
+    * @param {string} description Description text displayed
+    * when marker is clicked
+    */
+  setContent(description) {}
+
+  /**
+   * @param {google.maps.Map} map Map reference to display window
+   * @param {google.maps.Marker} marker Marker reference to
+   * window
+   */
+  open(map, marker) {}
+};
+
+google.maps.event = {};
+/**
+   * @param {google.maps.Marker} marker Marker reference to
+   *                                    info window
+   * @param {string} event Event that google.maps.event
+   * will listen for
+   * @param {Function} handler Function that will create
+   * description for info window
+   */
+google.maps.event.addListener = (marker, event, handler) => {};
+
+describe('Map Marker and InfoWindow Display', () => {
+  const mapsJsonData = [
+    {'name': 'New York University',
+      'city': 'New York',
+      'isMainCampus': true,
+      'latitude': 40.729452,
+      'longitude': -73.997264},
+    {'name': 'Stanford University',
+      'city': 'Stanford',
+      'isMainCampus': true,
+      'latitude': 37.429434,
+      'longitude': -122.167359},
+  ];
+  const nyuLat = mapsJsonData[0]['latitude'];
+  const nyuLon = mapsJsonData[0]['longitude'];
+  const nyuDescription = mapsJsonData[0]['name'] +
+  ' is located in ' + mapsJsonData[0]['city'] +
+   '. This is the main campus.';
+  const stanLat = mapsJsonData[1]['latitude'];
+  const stanLon = mapsJsonData[1]['longitude'];
+  const stanDescription = mapsJsonData[1]['name'] +
+  ' is located in ' + mapsJsonData[1]['city'] +
+   '. This is the main campus.';
+  beforeEach(() => {
+    spyOn(google.maps.Map.prototype, 'emptyMethod');
+    spyOn(google.maps.LatLng.prototype, 'emptyMethod');
+    spyOn(google.maps.Marker.prototype, 'emptyMethod');
+    spyOn(google.maps.event, 'addListener');
+    spyOn(google.maps.InfoWindow.prototype, 'emptyMethod');
+    spyOn(google.maps.InfoWindow.prototype, 'setContent');
+    spyOn(google.maps.InfoWindow.prototype, 'open');
+  });
+  it('will add map content using data from JSON',
+      async () => {
+        spyOn(window, 'fetch')
+            .and.returnValue(Promise.resolve({json: () => mapsJsonData}));
+        await fetchData();
+        createMap(mapsJsonData);
+        expect(google.maps.LatLng.prototype.emptyMethod).toHaveBeenCalledWith(
+            nyuLat,
+            nyuLon,
+        );
+        expect(google.maps.LatLng.prototype.emptyMethod).toHaveBeenCalledWith(
+            stanLat,
+            stanLon,
+        );
+        expect(google.maps.event.addListener).toHaveBeenCalled();
+      });
+  it('will create descriptions for info windows', () => {
+    expect(setDescription(
+        mapsJsonData[0]['name'],
+        mapsJsonData[0]['city'],
+        mapsJsonData[0]['isMainCampus']))
+        .toEqual(nyuDescription);
+    expect(setDescription(
+        mapsJsonData[1]['name'],
+        mapsJsonData[1]['city'],
+        mapsJsonData[1]['isMainCampus']))
+        .toEqual(stanDescription);
+  });
+});
